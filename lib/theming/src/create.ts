e3c298a383b719785c8203b8aa27dcd0b6078b42
Michael Shilman <shilman@users.noreply.github.com>
2019-03-16 08:55:16 +0800

    Merge pull request #6120 from storybooks/fix/theming-brandImage
    FIX brandImage staying storybook's logo when theme is created with brandTitle

--- lib/theming/src/create.ts
+++ lib/theming/src/create.ts
@@ -165,7 +165,7 @@ export const convert = (inherit: ThemeVars = lightThemeVars): Theme => {
     brand: {
       title: brandTitle,
       url: brandUrl,
-      image: brandImage,
+      image: brandImage || brandTitle ? null : undefined,
     },
 
     code: createSyntax({
