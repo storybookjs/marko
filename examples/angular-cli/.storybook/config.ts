9698d775a3619eccc8d72aa1f67640343ee26321
Michael Shilman <michael@lab80.co>
2019-04-28 16:19:22 +0800

    Stories Load API for Angular

--- examples/angular-cli/.storybook/config.ts
+++ examples/angular-cli/.storybook/config.ts
@@ -1,4 +1,4 @@
-import { storiesOf, configure, addParameters } from '@storybook/angular';
+import { load, addParameters } from '@storybook/angular';
 import addCssWarning from '../src/cssWarning';
 import { Wrapper } from '@storybook/components';
 
@@ -23,66 +23,9 @@ if (module && module.hot && module.hot.dispose) {
   });
 }
 
-function importAll(req) {
-  const storyStore = window.__STORYBOOK_CLIENT_API__._storyStore; // eslint-disable-line no-undef, no-underscore-dangle
+// put welcome screen at the top of the list so it's the first one displayed
+// FIXME: require('../src/stories');
 
-  req.keys().forEach(filename => {
-    const fileExports = req(filename);
-
-    // An old-style story file
-    if (!fileExports.componentMeta) {
-      return;
-    }
-
-    const { componentMeta, default: mdxDefault, ...examples } = fileExports;
-
-    let componentOptions = componentMeta;
-    if (componentMeta.prototype && componentMeta.prototype.isReactComponent) {
-      componentOptions = { componentMeta };
-    }
-    const kindName = componentOptions.title || componentOptions.componentMeta.displayName;
-
-    if (previousExports[filename]) {
-      if (previousExports[filename] === fileExports) {
-        return;
-      }
-
-      // Otherwise clear this kind
-      storyStore.removeStoryKind(kindName);
-      storyStore.incrementRevision();
-    }
-
-    // We pass true here to avoid the warning about HMR. It's cool clientApi, we got this
-    const kind = storiesOf(kindName, true);
-
-    (componentOptions.decorators || []).forEach(decorator => {
-      kind.addDecorator(decorator);
-    });
-    if (componentOptions.parameters) {
-      kind.addParameters(componentOptions.parameters);
-    }
-
-    Object.keys(examples).forEach(key => {
-      const example = examples[key];
-      const { title = example.title || key, parameters } = example;
-      kind.add(title, example, parameters);
-    });
-
-    previousExports[filename] = fileExports;
-  });
-}
-
-function loadStories() {
-  // put welcome screen at the top of the list so it's the first one displayed
-  require('../src/stories');
-
-  // automatically import all story ts files that end with *.stories.ts
-  let req;
-  req = require.context('../src/stories', true, /\.stories\.ts$/);
-  importAll(req);
-
-  req = require.context('../src/stories', true, /\.stories\.mdx$/);
-  importAll(req);
-}
-
-configure(loadStories, module);
+// automatically import all story ts files that end with *.stories.ts
+load(require.context('../src/stories', true, /\.stories\.ts$/), module);
+load(require.context('../src/stories', true, /\.stories\.mdx$/), module);
