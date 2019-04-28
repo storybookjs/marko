5acc32d6cb5ddb54feb13d54277fb15644cae305
Michael Shilman <michael@lab80.co>
2019-04-28 16:07:44 +0800

    Stories Load API for React

--- examples/cra-mdx/.storybook/config.js
+++ examples/cra-mdx/.storybook/config.js
@@ -1,4 +1,4 @@
-import { storiesOf, configure, addParameters } from '@storybook/react';
+import { load, addParameters } from '@storybook/react';
 import { create } from '@storybook/theming';
 import { Wrapper } from '@storybook/components';
 import { Info } from './Info';
@@ -19,72 +19,5 @@ addParameters({
   docs: Info,
 });
 
-let previousExports = {};
-if (module && module.hot && module.hot.dispose) {
-  ({ previousExports = {} } = module.hot.data || {});
-
-  module.hot.dispose(data => {
-    // eslint-disable-next-line no-param-reassign
-    data.previousExports = previousExports;
-  });
-}
-
-function importAll(req) {
-  const storyStore = window.__STORYBOOK_CLIENT_API__._storyStore; // eslint-disable-line no-undef, no-underscore-dangle
-
-  req.keys().forEach(filename => {
-    const fileExports = req(filename);
-
-    // An old-style story file
-    if (!fileExports.componentMeta) {
-      return;
-    }
-
-    const { componentMeta, default: mdxDefault, ...examples } = fileExports;
-
-    let componentOptions = componentMeta;
-    if (componentMeta.prototype && componentMeta.prototype.isReactComponent) {
-      componentOptions = { componentMeta };
-    }
-    const kindName = componentOptions.title || componentOptions.componentMeta.displayName;
-
-    if (previousExports[filename]) {
-      if (previousExports[filename] === fileExports) {
-        return;
-      }
-
-      // Otherwise clear this kind
-      storyStore.removeStoryKind(kindName);
-      storyStore.incrementRevision();
-    }
-
-    // We pass true here to avoid the warning about HMR. It's cool clientApi, we got this
-    const kind = storiesOf(kindName, true);
-
-    (componentOptions.decorators || []).forEach(decorator => {
-      kind.addDecorator(decorator);
-    });
-    if (componentOptions.parameters) {
-      kind.addParameters(componentOptions.parameters);
-    }
-
-    Object.keys(examples).forEach(key => {
-      const example = examples[key];
-      const { title = example.title || key, parameters } = example;
-      kind.add(title, example, parameters);
-    });
-
-    previousExports[filename] = fileExports;
-  });
-}
-
-function loadStories() {
-  let req;
-  req = require.context('../src', true, /\.stories\.js$/);
-  importAll(req);
-
-  req = require.context('../src', true, /\.stories\.mdx$/);
-  importAll(req);
-}
-
-configure(loadStories, module);
+load(require.context('../src', true, /\.stories\.js$/), module);
+load(require.context('../src', true, /\.stories\.mdx$/), module);
