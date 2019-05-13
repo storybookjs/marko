7ddcc3d9b6e0172162fdae79ef5f012370155feb
Michael Shilman <michael@lab80.co>
2019-05-13 12:21:06 +0800

    Added complex prop types to PropRow stories

--- lib/components/src/blocks/PropsTable/PropRow.stories.tsx
+++ lib/components/src/blocks/PropsTable/PropRow.stories.tsx
@@ -44,7 +44,70 @@ const numberDef = {
   defaultValue: 0,
 };
 
+const objectDef = {
+  name: 'someObject',
+  type: { name: 'objectOf', value: { name: 'number' } },
+  required: false,
+  description: 'A simple `objectOf` propType.',
+  defaultValue: { value: '{ key: 1 }', computed: false },
+};
+
+const arrayDef = {
+  name: 'someOArray',
+  type: { name: 'arrayOf', value: { name: 'number' } },
+  required: false,
+  description: 'array of a certain type',
+  defaultValue: { value: '[1, 2, 3]', computed: false },
+};
+
+const complexDef = {
+  name: 'someComplex',
+  type: {
+    name: 'objectOf',
+    value: {
+      name: 'shape',
+      value: {
+        id: {
+          name: 'number',
+          description:
+            "Just an internal propType for a shape.\n It's also required, and as you can see it supports multi-line comments!",
+          required: true,
+        },
+        func: {
+          name: 'func',
+          description: 'A simple non-required function',
+          required: false,
+        },
+        arr: {
+          name: 'arrayOf',
+          value: {
+            name: 'shape',
+            value: {
+              index: {
+                name: 'number',
+                description: '5-level deep propType definition and still works.',
+                required: true,
+              },
+            },
+          },
+          description: 'An `arrayOf` shape',
+          required: false,
+        },
+      },
+    },
+  },
+  required: false,
+  description: 'A very complex `objectOf` propType.',
+  defaultValue: {
+    value: '{\n  thing: {\n    id: 2,\n    func: () => {},\n    arr: [],\n  },\n}',
+    computed: false,
+  },
+};
+
 export const string = () => <PropRow row={stringDef} />;
 export const longName = () => <PropRow row={longNameDef} />;
 export const longDesc = () => <PropRow row={longDescDef} />;
 export const number = () => <PropRow row={numberDef} />;
+export const objectOf = () => <PropRow row={objectDef} />;
+export const arrayOf = () => <PropRow row={arrayDef} />;
+export const complex = () => <PropRow row={complexDef} />;
