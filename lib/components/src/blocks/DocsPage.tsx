d131a4b55647ec09fdb8c352ee73f83bc127fc01
domyen <dom@hichroma.com>
2019-05-13 14:39:04 +0800

    DocsPage style refinements

--- lib/components/src/blocks/DocsPage.tsx
+++ lib/components/src/blocks/DocsPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { styled } from '@storybook/theming';
+import { transparentize } from 'polished';
 
 import { Preview, PreviewProps } from './Preview';
 import { PropsTable, PropsTableProps } from './PropsTable/PropsTable';
@@ -7,15 +8,42 @@ import { Source, SourceProps } from './Source';
 import { Description, DescriptionProps } from './Description';
 import { DocumentFormatting } from '../typography/DocumentFormatting';
 
+export const breakpoint = 600;
+export const pageMargin = '5.55555';
+
 const Title = styled.h1(({ theme }) => ({
-  // fontSize: theme.typography.size.l1,
-  // fontWeight: theme.typography.weight.black,
+  // overrides h1 in DocumentFormatting
+  '&&': {
+    fontSize: theme.typography.size.m3,
+    lineHeight: '32px',
+
+    [`@media (min-width: ${breakpoint * 1}px)`]: {
+      fontSize: theme.typography.size.l1,
+      lineHeight: '36px',
+    },
+  },
 }));
 
 const Subtitle = styled.h2(({ theme }) => ({
   // overrides h2 in DocumentFormatting
-  fontSize: theme.typography.size.m3,
-  fontWeight: theme.typography.weight.black,
+  '&&': {
+    fontWeight: theme.typography.weight.regular,
+    fontSize: theme.typography.size.s3,
+    lineHeight: '20px',
+    borderBottom: 'none',
+    marginBottom: '15px',
+
+    [`@media (min-width: ${breakpoint * 1}px)`]: {
+      fontSize: theme.typography.size.m1,
+      lineHeight: '28px',
+      marginBottom: '25px',
+    },
+  },
+
+  color:
+    theme.base === 'light'
+      ? transparentize(0.25, theme.color.defaultText)
+      : transparentize(0.25, theme.color.defaultText),
 }));
 
 export const Content = styled(DocumentFormatting)({
@@ -28,8 +56,9 @@ const Wrapper = styled.div(({ theme }) => ({
   display: 'flex',
   justifyContent: 'center',
   minHeight: '100vh',
-  paddingTop: '4rem',
-  paddingBottom: '4rem',
+  padding: '4rem 20px',
+
+  [`@media (min-width: ${breakpoint * 1}px)`]: {},
 }));
 
 export interface DocsPageProps {
