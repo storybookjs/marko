9933cf89f198195909aac7a801601459c0383c2b
Michael Shilman <michael@lab80.co>
2019-05-25 15:09:25 -0700

    Addon-docs: Add tests for module stories format in typescript

--- examples/cra-mdx/src/stories/legacy.stories.tsx
+++ examples/cra-mdx/src/stories/legacy.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import TypescriptButton from './TypescriptButton';
 
-storiesOf('Legacy|TypescriptButton', module)
+storiesOf('Typescript|Legacy', module)
   .addParameters({ component: TypescriptButton })
   .add('small', (): any => <TypescriptButton variant="small" />)
   .add('large', (): any => <TypescriptButton variant="large" />);
