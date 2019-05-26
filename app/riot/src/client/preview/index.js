a440db61f54ecdd807638419c4a0432b6c154b2f
Michael Shilman <michael@lab80.co>
2019-05-25 22:36:03 -0700

    Core: add framework parameter to every storiesOf

--- app/riot/src/client/preview/index.js
+++ app/riot/src/client/preview/index.js
@@ -5,10 +5,9 @@ import riot, { tag2, mount as vendorMount } from 'riot';
 import render from './render';
 import { compileNow as unboundCompileNow, asCompiledCode } from './compileStageFunctions';
 
-const { load, clientApi, configApi, forceReRender } = start(render);
+const { load: coreLoad, clientApi, configApi, forceReRender } = start(render);
 
 export const {
-  storiesOf,
   setAddon,
   addDecorator,
   addParameters,
@@ -17,7 +16,11 @@ export const {
   raw,
 } = clientApi;
 
+const framework = 'riot';
+export const storiesOf = (...args) => clientApi.storiesOf(...args).addParameters({ framework });
+export const load = (...args) => coreLoad(...args, framework);
+
 export const { configure } = configApi;
 const mount = vendorMount.bind(riot, '#root');
 const compileNow = unboundCompileNow.bind(null, tag2);
-export { load, forceReRender, render, tag2 as tag, mount, compileNow, asCompiledCode };
+export { forceReRender, render, tag2 as tag, mount, compileNow, asCompiledCode };
