a67a66b9eb3958703d85430dbd911dd29a226788
libetl <libe4@free.fr>
2019-06-02 21:48:03 +0200

    trying to make the default export work with storySource #6941

--- examples/cra-mdx/.storybook/webpack.config.js
+++ examples/cra-mdx/.storybook/webpack.config.js
@@ -1,17 +1,17 @@
 const path = require('path');
 
 module.exports = async ({ config }) => {
-  // config.module.rules.push({
-  //   test: /\.stories\.[tj]sx?$/,
-  //   use: [
-  //     {
-  //       loader: require.resolve('@storybook/source-loader'),
-  //       options: { injectParameters: true },
-  //     },
-  //   ],
-  //   include: [path.resolve(__dirname, '../src')],
-  //   enforce: 'pre',
-  // });
+  config.module.rules.push({
+    test: /\.stories\.[tj]sx?$/,
+    use: [
+      {
+        loader: require.resolve('@storybook/source-loader'),
+        options: { injectParameters: true },
+      },
+    ],
+    include: [path.resolve(__dirname, '../src')],
+    enforce: 'pre',
+  });
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
