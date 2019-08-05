e6c05b0ba7902856655c1a44e49b79c8e6f27ec3
Michael Shilman <michael@lab80.co>
2019-08-06 01:59:12 +0800

    Svelte: Add MDX example

--- examples/svelte-kitchen-sink/.storybook/config.js
+++ examples/svelte-kitchen-sink/.storybook/config.js
@@ -8,4 +8,4 @@ addParameters({
   },
 });
 
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+configure(require.context('../src/stories', true, /\.stories\.(js|mdx)$/), module);
