93d4142be3604252b744e25aa8ef0a2fae90b07a
Kai Roeder <roeder.kai@gmail.com>
2019-11-08 23:38:30 +0100

    update angular fixture names

--- lib/cli/test/fixtures/angular-cli-v9/src/app/app.component.ts
+++ lib/cli/test/fixtures/angular-cli-v9/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng9';
+  title = 'angular-cli-v9';
 }
