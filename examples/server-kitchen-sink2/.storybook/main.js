9987b50367e8874f0f3e7454d9b0ea4831350f94
Jon Palmer <jonspalmer@gmail.com>
2020-01-02 13:02:24 -0500

    Update to new main.js format

--- examples/server-kitchen-sink2/.storybook/main.js
+++ examples/server-kitchen-sink2/.storybook/main.js
@@ -1,15 +1,13 @@
 module.exports = {
-  // this dirname is because we run tests from project root
-  stories: [`${__dirname}/../stories/*.stories.*`],
-
-  presets: ['@storybook/addon-docs/preset'],
+  stories: ['../stories/**/*.stories.json'],
   addons: [
-    '@storybook/addon-a11y/register',
-    '@storybook/addon-actions/register',
-    '@storybook/addon-backgrounds/register',
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-links/register',
-    '@storybook/addon-notes/register',
-    '@storybook/addon-options/register',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-notes',
+    '@storybook/addon-options',
   ],
 };
