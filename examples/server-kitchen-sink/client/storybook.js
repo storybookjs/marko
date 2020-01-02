7099e18cff7a4632448975c7391a38563a970e6f
Jon Palmer <jonspalmer@gmail.com>
2020-01-02 13:01:07 -0500

    Wire up the knobs

--- examples/server-kitchen-sink/client/storybook.js
+++ examples/server-kitchen-sink/client/storybook.js
@@ -1,32 +1,19 @@
 import camelcase from 'camelcase';
-import { configure, setFetchStoryHtml } from '@storybook/server';
-import stories from '../server/stories';
+import { configure } from '@storybook/server';
+
+import stories from './storybook.stories';
 
 const port = process.env.PORT || 8080;
 
 const fetchHtml = async (id, params) => {
   const [component, story] = id.split('--').map(s => camelcase(s));
 
-  const url = `http://localhost:${port}/storybook_preview/${component}/${story}`;
+  const url = new URL(`http://localhost:${port}/storybook_preview/${component}/${story}`);
+  url.search = new URLSearchParams(params).toString();
 
   // eslint-disable-next-line no-undef
   const response = await fetch(url);
   return response.text();
 };
 
-const storyBookStories = Object.keys(stories).map(component => {
-  const storybookDescription = {
-    default: {
-      title: component,
-    },
-  };
-  Object.keys(stories[component]).forEach(storyName => {
-    storybookDescription[storyName] = () => {};
-  });
-
-  return storybookDescription;
-});
-
-setFetchStoryHtml(fetchHtml);
-
-configure(() => storyBookStories, module);
+configure(() => stories, module, { fetchStoryHtml: fetchHtml });
