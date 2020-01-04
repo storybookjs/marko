ffd2f54be9d98dc579590e7c3a3c79dc2a1443e2
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2020-01-04 18:29:29 +0000

    Official example: try moving options to `manager.js`

--- examples/official-storybook/manager.js
+++ examples/official-storybook/manager.js
@@ -1,3 +1,13 @@
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+
 import addHeadWarning from './head-warning';
 
 addHeadWarning('manager-head-not-loaded', 'Manager head not loaded');
+
+addons.setConfig({
+  showRoots: true,
+  theme: themes.light, // { base: 'dark', brandTitle: 'Storybook!' },
+  storySort: (a, b) =>
+    a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+});
--- examples/official-storybook/preview.js
+++ examples/official-storybook/preview.js
@@ -44,12 +44,6 @@ addParameters({
       restoreScroll: true,
     },
   },
-  options: {
-    showRoots: true,
-    theme: themes.light, // { base: 'dark', brandTitle: 'Storybook!' },
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-  },
   backgrounds: [
     { name: 'storybook app', value: themes.light.appBg, default: true },
     { name: 'light', value: '#eeeeee' },
