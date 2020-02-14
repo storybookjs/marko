a83e9b10a7f88718c2cd024f3c883030a9aa65f8
Norbert de Langen <ndelangen@me.com>
2020-02-14 13:19:27 +0100

    FIX zooming on refs

--- lib/ui/src/components/preview/iframe.tsx
+++ lib/ui/src/components/preview/iframe.tsx
@@ -12,7 +12,7 @@ const StyledIframe = styled.iframe({
   height: '100%',
   width: '100%',
   border: '0 none',
-  transition: 'all .3s, background-position 0s',
+  transition: 'all .3s, background-position 0s, visibility 0s',
   backgroundPosition: '-1px -1px, -1px -1px, -1px -1px, -1px -1px',
 });
 
@@ -34,19 +34,9 @@ export class IFrame extends Component<IFrameProps & IframeHTMLAttributes<HTMLIFr
 
   shouldComponentUpdate(nextProps: IFrameProps) {
     const { scale } = this.props;
+
     if (scale !== nextProps.scale) {
-      if (window.navigator.userAgent.indexOf(FIREFOX_BROWSER) !== -1) {
-        this.setIframeBodyStyle({
-          width: `${nextProps.scale * 100}%`,
-          height: `${nextProps.scale * 100}%`,
-          transform: `scale(${1 / nextProps.scale})`,
-          transformOrigin: 'top left',
-        });
-      } else {
-        this.setIframeBodyStyle({
-          zoom: 1 / nextProps.scale,
-        });
-      }
+      this.setIframeInnerZoom(nextProps.scale);
     }
 
     // this component renders an iframe, which gets updates via post-messages
@@ -54,8 +44,32 @@ export class IFrame extends Component<IFrameProps & IframeHTMLAttributes<HTMLIFr
     return false;
   }
 
-  setIframeBodyStyle(style: CSSProperties) {
-    return Object.assign(this.iframe.contentDocument.body.style, style);
+  setIframeInnerZoom(scale: number) {
+    try {
+      if (window.navigator.userAgent.indexOf(FIREFOX_BROWSER) !== -1) {
+        Object.assign(this.iframe.contentDocument.body.style, {
+          width: `${scale * 100}%`,
+          height: `${scale * 100}%`,
+          transform: `scale(${1 / scale})`,
+          transformOrigin: 'top left',
+        });
+      } else {
+        Object.assign(this.iframe.contentDocument.body.style, {
+          zoom: 1 / scale,
+        });
+      }
+    } catch (e) {
+      this.setIframeZoom(scale);
+    }
+  }
+
+  setIframeZoom(scale: number) {
+    Object.assign(this.iframe.style, {
+      width: `${scale * 100}%`,
+      height: `${scale * 100}%`,
+      transform: `scale(${1 / scale})`,
+      transformOrigin: 'top left',
+    });
   }
 
   render() {
