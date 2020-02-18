W427697/Discord-

Ensure the story re-renders when the state changes

lib/client-api/src/story_store.ts
@@ -308,7 +308,10 @@ export default class StoryStore extends EventEmitter {
     const { state } = this._data[id];
     this._data[id].state = { ...state, ...newState };
 
+    // TODO: Sort out what is going on with both the store and the channel being event emitters.
+    // It has something to do with React Native, but need to get to the bottom of it
     this._channel.emit(Events.STORY_STATE_CHANGED, id, this._data[id].state);
+    this.emit(Events.STORY_STATE_CHANGED, id, this._data[id].state);
   }
 
   // This API is a reimplementation of Storybook's original getStorybook() API.

storybookjs/marko

lib/client-api/src/story_store.ts
@@ -358,7 +358,10 @@ export default class StoryStore extends EventEmitter {
     const { state } = this._stories[id];
     this._stories[id].state = { ...state, ...newState };
 
+    // TODO: Sort out what is going on with both the store and the channel being event emitters.
+    // It has something to do with React Native, but need to get to the bottom of it
     this._channel.emit(Events.STORY_STATE_CHANGED, id, this._stories[id].state);
+    this.emit(Events.STORY_STATE_CHANGED, id, this._stories[id].state);
   }
 
   // This API is a reimplementation of Storybook's original getStorybook() API.