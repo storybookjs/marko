7cdce26c53c171f2430c25da02e8049efb73d1a1
Norbert de Langen <ndelangen@me.com>
2020-02-18 09:51:41 +0100

    CLEANUP

--- addons/graphql/src/manager.tsx
+++ addons/graphql/src/manager.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent } from 'react';
-import PropTypes from 'prop-types';
 import GraphiQL from 'graphiql';
 import 'graphiql/graphiql.css';
 
@@ -31,9 +30,6 @@ const GQL: FunctionComponent<GQLProps> = ({ active }) => {
       }}
     </Consumer>
   ) : null;
-};
-GQL.propTypes = {
-  active: PropTypes.bool.isRequired,
 };
 
 export default GQL;
