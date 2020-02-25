9c950476afdcae5d0104c1c33c55b74fcb7b690a
Norbert de Langen <ndelangen@me.com>
2020-02-25 15:42:47 +0100

    Revert "MIGRATE addon-events"
    This reverts commit b227f774e4c0365823b3ca571f2a19d1ab1aa11e.

--- addons/events/src/components/Panel.tsx
+++ addons/events/src/components/Panel.tsx
@@ -1,7 +1,8 @@
-import React, { useState, FunctionComponent, useCallback } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import { styled } from '@storybook/theming';
-import { useChannel } from '@storybook/api';
+import { API } from '@storybook/api';
 
 import { EVENTS } from '../constants';
 import Event from './Event';
@@ -16,26 +17,56 @@ const Wrapper = styled.div({
 
 interface EventsPanelProps {
   active: boolean;
+  api: API;
+}
+interface EventsPanelState {
+  events: EventType[];
 }
 
-const EventsPanel: FunctionComponent<EventsPanelProps> = ({ active }) => {
-  const [state, setState] = useState<EventType[]>([]);
+export default class EventsPanel extends Component<EventsPanelProps, EventsPanelState> {
+  static propTypes = {
+    active: PropTypes.bool.isRequired,
+    api: PropTypes.shape({
+      emit: PropTypes.func,
+      off: PropTypes.func,
+      on: PropTypes.func,
+    }).isRequired,
+  };
 
-  const emit = useChannel({
-    [EVENTS.ADD]: (events: EventType[]) => setState(events),
-  });
+  state: EventsPanelState = {
+    events: [],
+  };
 
-  const onEmit = useCallback((event: OnEmitEvent) => {
-    emit(EVENTS.EMIT, event);
-  }, []);
+  componentDidMount() {
+    const { api } = this.props;
 
-  return active ? (
-    <Wrapper>
-      {state.map(event => (
-        <Event key={event.name} {...event} onEmit={onEmit} />
-      ))}
-    </Wrapper>
-  ) : null;
-};
+    api.on(EVENTS.ADD, this.onAdd);
+  }
 
-export default EventsPanel;
+  componentWillUnmount() {
+    const { api } = this.props;
+
+    api.off(EVENTS.ADD, this.onAdd);
+  }
+
+  onAdd = (events: EventType[]) => {
+    this.setState({ events });
+  };
+
+  onEmit = (event: OnEmitEvent) => {
+    const { api } = this.props;
+    api.emit(EVENTS.EMIT, event);
+  };
+
+  render() {
+    const { events } = this.state;
+    const { active } = this.props;
+    return active ? (
+      <Wrapper>
+        {events.map(event => (
+          <Event key={event.name} {...event} onEmit={this.onEmit} />
+        ))}
+      </Wrapper>
+    ) : null;
+  }
+}
--- addons/events/src/manager.tsx
+++ addons/events/src/manager.tsx
@@ -5,10 +5,10 @@ import Panel from './components/Panel';
 import { ADDON_ID, PANEL_ID, PARAM_KEY } from './constants';
 
 export function register() {
-  addons.register(ADDON_ID, () => {
+  addons.register(ADDON_ID, api => {
     addons.addPanel(PANEL_ID, {
       title: 'Events',
-      render: ({ active, key }) => <Panel key={key} active={active} />,
+      render: ({ active, key }) => <Panel key={key} api={api} active={active} />,
       paramKey: PARAM_KEY,
     });
   });
