71876af37d4fe48b40ba7544983cf240f303dc87
Tom Coleman <tom@thesnail.org>
2020-02-27 13:52:15 +1100

    Re-render when global args change

--- lib/addons/src/hooks.ts
+++ lib/addons/src/hooks.ts
@@ -5,6 +5,7 @@ import {
   STORY_RENDERED,
   DOCS_RENDERED,
   CHANGE_STORY_ARGS,
+  CHANGE_GLOBAL_ARGS,
 } from '@storybook/core-events';
 import { addons } from './index';
 import { StoryGetter, StoryContext, Args } from './types';
@@ -426,4 +427,17 @@ export function useArgs(): [Args, (newArgs: Args) => void] {
   );
 
   return [args, updateArgs];
+}
+
+/* Returns current value of global args */
+export function useGlobalArgs(): [Args, (newGlobalArgs: Args) => void] {
+  const channel = addons.getChannel();
+  const { globalArgs } = useStoryContext();
+
+  const updateGlobalArgs = useCallback(
+    (newGlobalArgs: Args) => channel.emit(CHANGE_GLOBAL_ARGS, newGlobalArgs),
+    [channel]
+  );
+
+  return [globalArgs, updateGlobalArgs];
 }
