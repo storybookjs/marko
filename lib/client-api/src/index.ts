78949ef0b5d193dd55d0986fe3893ede9941dee0
Michael Shilman <michael@lab80.co>
2020-03-01 15:06:32 +0800

    Export combineParameters and types from client-api

--- lib/client-api/src/index.ts
+++ lib/client-api/src/index.ts
@@ -1,5 +1,6 @@
 import ClientApi, { addDecorator, addParameters, addParameterEnhancer } from './client_api';
 import { defaultDecorateStory } from './decorators';
+import { combineParameters } from './parameters';
 import StoryStore from './story_store';
 import ConfigApi from './config_api';
 import pathToId from './pathToId';
@@ -7,12 +8,14 @@ import pathToId from './pathToId';
 import { getQueryParams, getQueryParam } from './queryparams';
 
 export * from './hooks';
+export * from './types';
 
 export {
   ClientApi,
   addDecorator,
   addParameters,
   addParameterEnhancer,
+  combineParameters,
   StoryStore,
   ConfigApi,
   defaultDecorateStory,
