1722f54b7ec415df9a69368a12dbef1e8b102a05
Norbert de Langen <ndelangen@me.com>
2020-03-02 16:24:16 +0100

    ADD storySort to preview.js again

--- examples/official-storybook/preview.js
+++ examples/official-storybook/preview.js
@@ -40,6 +40,10 @@ addParameters({
       restoreScroll: true,
     },
   },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
   backgrounds: [
     { name: 'storybook app', value: themes.light.appBg, default: true },
     { name: 'light', value: '#eeeeee' },
