121c50a8b1159c5df9b7bac83e722f6b21a1f51d
Norbert de Langen <ndelangen@me.com>
2020-03-05 11:32:30 +0100

    FIX copy & eject functionality to work with refs

--- lib/ui/src/components/preview/tools/copy.tsx
+++ lib/ui/src/components/preview/tools/copy.tsx
@@ -5,30 +5,35 @@ import { Consumer, Combo } from '@storybook/api';
 import { Addon } from '@storybook/addons';
 import { stringifyQueryParams } from '../utils/stringifyQueryParams';
 
-const copyMapper = ({ state }: Combo) => ({
-  origin: state.location.origin,
-  pathname: state.location.pathname,
-  storyId: state.storyId,
-  baseUrl: 'iframe.html',
-  queryParams: state.customQueryParams,
-});
+const copyMapper = ({ state, api }: Combo) => {
+  const story = api.getData(state.storyId);
+  const ref = api.getRefs()[story && story.refId];
+
+  return {
+    baseUrl: ref
+      ? `${ref.url}/iframe.html`
+      : `${state.location.origin + state.location.pathname}iframe.html`,
+    storyId: story ? story.knownAs || story.id : null,
+    queryParams: state.customQueryParams,
+  };
+};
 
 export const copyTool: Addon = {
   title: 'copy',
   match: ({ viewMode }) => viewMode === 'story',
   render: () => (
     <Consumer filter={copyMapper}>
-      {({ baseUrl, storyId, origin, pathname, queryParams }) => (
-        <IconButton
-          key="copy"
-          onClick={() =>
-            copy(`${origin}${pathname}${baseUrl}?id=${storyId}${stringifyQueryParams(queryParams)}`)
-          }
-          title="Copy canvas link"
-        >
-          <Icons icon="copy" />
-        </IconButton>
-      )}
+      {({ baseUrl, storyId, queryParams }) =>
+        storyId ? (
+          <IconButton
+            key="copy"
+            onClick={() => copy(`${baseUrl}?id=${storyId}${stringifyQueryParams(queryParams)}`)}
+            title="Copy canvas link"
+          >
+            <Icons icon="copy" />
+          </IconButton>
+        ) : null
+      }
     </Consumer>
   ),
 };
--- lib/ui/src/components/preview/tools/eject.tsx
+++ lib/ui/src/components/preview/tools/eject.tsx
@@ -4,27 +4,34 @@ import { Consumer, Combo } from '@storybook/api';
 import { Addon } from '@storybook/addons';
 import { stringifyQueryParams } from '../utils/stringifyQueryParams';
 
-const ejectMapper = ({ state }: Combo) => ({
-  baseUrl: 'iframe.html',
-  storyId: state.storyId,
-  queryParams: state.customQueryParams,
-});
+const ejectMapper = ({ state, api }: Combo) => {
+  const story = api.getData(state.storyId);
+  const ref = api.getRefs()[story && story.refId];
+
+  return {
+    baseUrl: ref ? `${ref.url}/iframe.html` : 'iframe.html',
+    storyId: story ? story.knownAs || story.id : null,
+    queryParams: state.customQueryParams,
+  };
+};
 
 export const ejectTool: Addon = {
   title: 'eject',
   match: ({ viewMode }) => viewMode === 'story',
   render: () => (
     <Consumer filter={ejectMapper}>
-      {({ baseUrl, storyId, queryParams }) => (
-        <IconButton
-          key="opener"
-          href={`${baseUrl}?id=${storyId}${stringifyQueryParams(queryParams)}`}
-          target="_blank"
-          title="Open canvas in new tab"
-        >
-          <Icons icon="share" />
-        </IconButton>
-      )}
+      {({ baseUrl, storyId, queryParams }) =>
+        storyId ? (
+          <IconButton
+            key="opener"
+            href={`${baseUrl}?id=${storyId}${stringifyQueryParams(queryParams)}`}
+            target="_blank"
+            title="Open canvas in new tab"
+          >
+            <Icons icon="share" />
+          </IconButton>
+        ) : null
+      }
     </Consumer>
   ),
 };
