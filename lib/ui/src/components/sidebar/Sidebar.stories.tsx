1d89d4a48782c8f2a981fddc397a5711e2dcc517
Norbert de Langen <ndelangen@me.com>
2020-03-09 18:09:38 +0100

    RENAME loading to isLoading

--- lib/ui/src/components/sidebar/Sidebar.stories.tsx
+++ lib/ui/src/components/sidebar/Sidebar.stories.tsx
@@ -17,4 +17,4 @@ export const simpleData = { menu, stories, storyId };
 export const loadingData = { menu, stories: {} };
 
 export const simple = () => <Sidebar menu={menu} stories={stories} storyId={storyId} />;
-export const loading = () => <Sidebar menu={menu} stories={{}} loading />;
+export const loading = () => <Sidebar menu={menu} stories={{}} isLoading />;
