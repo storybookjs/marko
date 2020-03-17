652d0bc103c2b90ef96fe1a45b9abb2335cff3fc
Norbert de Langen <ndelangen@me.com>
2020-03-17 12:27:41 +0100

    REMOVE support for setting options from refs

--- lib/api/src/modules/stories.ts
+++ lib/api/src/modules/stories.ts
@@ -289,12 +289,12 @@ export const init: ModuleFn = ({
     fullAPI.on(STORY_CHANGED, function handleStoryChange(storyId: string) {
       const { source }: { source: string } = this;
       const sourceType = getSourceType(source);
-      const refId = sourceType === 'external' ? fullAPI.findRef(source).id : undefined;
+      if (sourceType === 'local') {
+        const options = fullAPI.getParameters({ storyId, refId: undefined }, 'options');
 
-      const options = fullAPI.getParameters({ storyId, refId }, 'options');
-
-      if (options) {
-        fullAPI.setOptions(options);
+        if (options) {
+          fullAPI.setOptions(options);
+        }
       }
     });
 
