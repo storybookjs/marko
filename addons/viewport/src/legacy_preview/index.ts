612353a345a7989c77dd418fad0d8a3acbf7a150
Norbert de Langen <ndelangen@me.com>
2020-03-26 10:31:46 +0100

    REMOVE deprecated APIs from addon/viewport

--- addons/viewport/src/legacy_preview/index.ts
+++ addons/viewport/src/legacy_preview/index.ts
@@ -1,6 +1,1 @@
-import deprecate from 'util-deprecate';
-
 export { INITIAL_VIEWPORTS, DEFAULT_VIEWPORT, MINIMAL_VIEWPORTS } from '../defaults';
-
-export const configureViewport = deprecate(() => {},
-'configureViewport is no longer supported, use .addParameters({ viewport }) instead');
