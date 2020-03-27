6fb4266c8af2eb1e7622188306c2a639c2652ca9
Filipp Riabchun <talpa@yandex.ru>
2020-03-27 22:25:01 +0100

    Merge pull request #10223 from rileylnapier/patch-3
    Storyshots: Fix mdx transform

--- addons/docs/jest-transform-mdx.js
+++ addons/docs/jest-transform-mdx.js
@@ -7,20 +7,6 @@ const createCompiler = require('./mdx-compiler-plugin');
 
 const compilers = [createCompiler({})];
 
-const getNextTransformer = (filename, config) => {
-  const extension = path.extname(filename);
-  const jsFileName = `${filename.slice(0, -extension.length)}.js`;
-  const self = config.transform.find(([pattern]) => new RegExp(pattern).test(filename));
-  const jsTransforms = config.transform.filter(([pattern]) => new RegExp(pattern).test(jsFileName));
-  return new ScriptTransformer({
-    ...config,
-    transform: [
-      ...config.transform.filter(entry => entry !== self),
-      ...jsTransforms.map(([pattern, ...rest]) => [self[0], ...rest]),
-    ],
-  });
-};
-
 module.exports = {
   process(src, filename, config, { instrument }) {
     const result = dedent`
@@ -29,6 +15,10 @@ module.exports = {
       import { mdx } from '@mdx-js/react'
       ${mdx.sync(src, { compilers, filepath: filename })}
     `;
-    return getNextTransformer(filename, config).transformSource(filename, result, instrument);
+    
+    const extension = path.extname(filename);
+    const jsFileName = `${filename.slice(0, -extension.length)}.js`;
+    
+    return new ScriptTransformer(config).transformSource(jsFileName, result, instrument);
   },
 };
