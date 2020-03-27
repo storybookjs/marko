5a60eb240cacc49e26a20fc112a254039f40782c
Riley <rileylnapier@gmail.com>
2020-03-27 13:20:13 -0700

    Update jest-transform-mdx.js

--- addons/docs/jest-transform-mdx.js
+++ addons/docs/jest-transform-mdx.js
@@ -4,22 +4,7 @@ const { ScriptTransformer } = require('@jest/transform');
 const { dedent } = require('ts-dedent');
 
 const createCompiler = require('./mdx-compiler-plugin');
-
 const compilers = [createCompiler({})];
-
-const getNextTransformer = (filename, config) => {
-  const extension = path.extname(filename);
-  const jsFileName = `${filename.slice(0, -extension.length)}.js`;
-  const self = config.transform.find(([pattern]) => new RegExp(pattern).test(filename));
-  const jsTransforms = config.transform.filter(([pattern]) => new RegExp(pattern).test(jsFileName));
-  return new ScriptTransformer({
-    ...config,
-    transform: [
-      ...config.transform.filter(entry => entry !== self),
-      ...jsTransforms.map(([pattern, ...rest]) => [self[0], ...rest]),
-    ],
-  });
-};
 
 module.exports = {
   process(src, filename, config, { instrument }) {
@@ -33,6 +18,6 @@ module.exports = {
     const extension = path.extname(filename);
     const jsFileName = `${filename.slice(0, -extension.length)}.js`;
     
-    return getNextTransformer(filename, config).transformSource(jsFileName, result, instrument);
+    return new ScriptTransformer(config).transformSource(jsFileName, result, instrument);
   },
 };
