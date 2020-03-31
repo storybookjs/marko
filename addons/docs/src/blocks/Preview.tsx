e188a46944a8b3d4ebfa57d6b3f394fc91cdfd47
Tom Nolan <tsnolan@outlook.com>
2020-03-31 15:56:32 -0400

    Add resetStyles prop to addon-docs Preview component

--- addons/docs/src/blocks/Preview.tsx
+++ addons/docs/src/blocks/Preview.tsx
@@ -1,8 +1,22 @@
-import React, { FunctionComponent, ReactElement, ReactNode, ReactNodeArray } from 'react';
+import React, {
+  createElement,
+  ElementType,
+  FunctionComponent,
+  ReactElement,
+  ReactNode,
+  ReactNodeArray,
+} from 'react';
+import { MDXProvider } from '@mdx-js/react';
 import { toId, storyNameFromExport } from '@storybook/csf';
+import { components as docsComponents } from '@storybook/components/html';
 import { Preview as PurePreview, PreviewProps as PurePreviewProps } from '@storybook/components';
 import { getSourceProps } from './Source';
 import { DocsContext, DocsContextProps } from './DocsContext';
+
+const resetComponents: Record<string, ElementType> = {};
+Object.keys(docsComponents).forEach(key => {
+  resetComponents[key] = (props: any) => createElement(key, props);
+});
 
 export enum SourceState {
   OPEN = 'open',
@@ -13,6 +27,7 @@ export enum SourceState {
 type PreviewProps = PurePreviewProps & {
   withSource?: SourceState;
   mdxSource?: string;
+  resetStyles?: boolean;
 };
 
 const getPreviewProps = (
@@ -38,7 +53,7 @@ const getPreviewProps = (
     (c: ReactElement) => c.props && (c.props.id || c.props.name)
   ) as ReactElement[];
   const targetIds = stories.map(
-    (s) =>
+    s =>
       s.props.id ||
       toId(
         mdxComponentMeta.id || mdxComponentMeta.title,
@@ -53,11 +68,15 @@ const getPreviewProps = (
   };
 };
 
-export const Preview: FunctionComponent<PreviewProps> = (props) => (
+export const Preview: FunctionComponent<PreviewProps> = props => (
   <DocsContext.Consumer>
-    {(context) => {
+    {context => {
       const previewProps = getPreviewProps(props, context);
-      return <PurePreview {...previewProps}>{props.children}</PurePreview>;
+      return (
+        <MDXProvider components={props.resetStyles ? resetComponents : docsComponents}>
+          <PurePreview {...previewProps}>{props.children}</PurePreview>
+        </MDXProvider>
+      );
     }}
   </DocsContext.Consumer>
 );
