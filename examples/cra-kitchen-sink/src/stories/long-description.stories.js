2ce9723ed15e747914ad233af5555c70e5cd2920
Norbert de Langen <ndelangen@me.com>
2020-04-03 13:14:17 +0200

    REMOVE addon-centered examples

--- examples/cra-kitchen-sink/src/stories/long-description.stories.js
+++ examples/cra-kitchen-sink/src/stories/long-description.stories.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import centered from '@storybook/addon-centered/react';
 import { Button } from '@storybook/react/demo';
 
 export default {
   title: 'Some really long story kind description',
-  decorators: [centered],
 };
 
 export const Story1 = () => <Button onClick={action('clicked')}>Hello Button</Button>;
