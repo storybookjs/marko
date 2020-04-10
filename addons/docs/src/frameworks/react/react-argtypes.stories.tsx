804b4953e9dbebabe11d7d73d8ccb0badda41785
Michael Shilman <michael@lab80.co>
2020-04-10 12:30:07 +0800

    Beef up ArgTypes stories

--- addons/docs/src/frameworks/react/react-argtypes.stories.tsx
+++ addons/docs/src/frameworks/react/react-argtypes.stories.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { ArgsTable } from '@storybook/components';
+import { ArgsTable, ArgsTableProps } from '@storybook/components';
 import { action } from '@storybook/addon-actions';
 import { Args } from '@storybook/api';
 import { combineParameters } from '@storybook/client-api';
 
 import { extractArgTypes } from './extractArgTypes';
 import { inferControls } from '../common/inferControls';
+import { Component } from '../../blocks';
 
-const fixtures = [
+const argsTableProps = (component: Component): ArgsTableProps => {
+  const argTypes = extractArgTypes(component);
+  const args = Object.keys(argTypes).reduce((acc, key) => {
+    acc[key] = null;
+    return acc;
+  }, {} as Args);
+  const controls = inferControls(argTypes);
+  const rows = combineParameters(argTypes, controls);
+  return { rows, args };
+};
+
+const typescriptFixtures = [
+  'aliases',
+  'arrays',
+  'enums',
+  'functions',
+  'interfaces',
+  'intersections',
+  'records',
+  'scalars',
+  'tuples',
+  'unions',
+];
+
+const typescriptStories = storiesOf('ArgTypes/TypeScript', module);
+typescriptFixtures.forEach((fixture) => {
+  // eslint-disable-next-line import/no-dynamic-require, global-require, no-shadow
+  const { Component } = require(`../../lib/sbtypes/__testfixtures__/typescript/${fixture}`);
+  const props = argsTableProps(Component);
+
+  typescriptStories.add(fixture, () => <ArgsTable {...props} updateArgs={action('updateArgs')} />);
+});
+
+const proptypesFixtures = ['arrays', 'enums', 'misc', 'objects', 'react', 'scalars'];
+
+const proptypesStories = storiesOf('ArgTypes/PropTypes', module);
+proptypesFixtures.forEach((fixture) => {
+  // eslint-disable-next-line import/no-dynamic-require, global-require, no-shadow
+  const { Component } = require(`../../lib/sbtypes/__testfixtures__/proptypes/${fixture}`);
+  const props = argsTableProps(Component);
+
+  proptypesStories.add(fixture, () => <ArgsTable {...props} updateArgs={action('updateArgs')} />);
+});
+
+const issuesFixtures = [
   'js-class-component',
   'ts-function-component',
   '9399-js-proptypes-shape',
@@ -36,21 +81,11 @@ const fixtures = [
   '9922-ts-component-props',
 ];
 
-const stories = storiesOf('Properties/ArgTypes', module);
-
-fixtures.forEach((fixture) => {
+const issuesStories = storiesOf('ArgTypes/Issues', module);
+issuesFixtures.forEach((fixture) => {
   // eslint-disable-next-line import/no-dynamic-require, global-require
   const { component } = require(`./__testfixtures__/${fixture}/input`);
-  const argTypes = extractArgTypes(component);
-  const args = Object.keys(argTypes).reduce((acc, key) => {
-    acc[key] = null;
-    return acc;
-  }, {} as Args);
-  const controls = inferControls(argTypes);
-  const rows = combineParameters(argTypes, controls);
+  const props = argsTableProps(component);
 
-  stories.add(fixture, () => {
-    console.log({ argTypes });
-    return <ArgsTable rows={rows} args={args} updateArgs={action('updateArgs')} />;
-  });
+  issuesStories.add(fixture, () => <ArgsTable {...props} updateArgs={action('updateArgs')} />);
 });
