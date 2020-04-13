c6ae997dcd2e44951b8ab08529866cf7635d98ed
Michael Shilman <michael@lab80.co>
2020-04-14 04:23:24 +0800

    ArgsTable: Fix and test extraction logic

--- addons/docs/src/frameworks/common/enhanceArgTypes.test.ts
+++ addons/docs/src/frameworks/common/enhanceArgTypes.test.ts
@@ -1,31 +1,33 @@
-import { ArgType } from '@storybook/api';
+import { ArgType, ArgTypes, Args } from '@storybook/api';
 import { enhanceArgTypes } from './enhanceArgTypes';
-import { extractArgTypes } from '../angular';
 
 expect.addSnapshotSerializer({
   print: (val: any) => JSON.stringify(val, null, 2),
-  test: (val) => true,
+  test: (val) => typeof val !== 'string',
 });
 
 const enhance = ({
   argType,
   arg,
-  extractedArgType,
+  extractedArgTypes,
+  storyFn = (args: Args) => 0,
 }: {
   argType?: ArgType;
   arg?: any;
-  extractedArgType?: ArgType;
+  extractedArgTypes?: ArgTypes;
+  storyFn?: any;
 }) => {
   const context = {
     id: 'foo--bar',
     kind: 'foo',
     name: 'bar',
+    storyFn,
     parameters: {
       component: 'dummy',
       docs: {
-        extractArgTypes: () => ({ input: extractedArgType }),
+        extractArgTypes: extractedArgTypes && (() => extractedArgTypes),
       },
-      argTypes: {
+      argTypes: argType && {
         input: argType,
       },
     },
@@ -34,20 +36,21 @@ const enhance = ({
     },
     globalArgs: {},
   };
-  const result = enhanceArgTypes(context);
-  return result?.argTypes?.input;
+  return enhanceArgTypes(context);
 };
 
 describe('enhanceArgTypes', () => {
-  describe('argTypes input', () => {
-    it('number', () => {
+  describe('no-args story function', () => {
+    it('should no-op', () => {
       expect(
         enhance({
-          argType: { type: { name: 'number' } },
-        })
+          argType: { foo: 'unmodified', type: { name: 'number' } },
+          storyFn: () => 0,
+        }).input
       ).toMatchInlineSnapshot(`
         {
           "name": "input",
+          "foo": "unmodified",
           "type": {
             "name": "number"
           }
@@ -55,86 +58,260 @@ describe('enhanceArgTypes', () => {
       `);
     });
   });
-  describe('args input', () => {
-    it('number', () => {
-      expect(enhance({ arg: 5 })).toMatchInlineSnapshot(`
-        {
-          "name": "input",
-          "type": {
-            "name": "number"
-          }
-        }
-      `);
-    });
-  });
+  describe('args story function', () => {
+    describe('single-source input', () => {
+      describe('argTypes input', () => {
+        it('number', () => {
+          expect(
+            enhance({
+              argType: { type: { name: 'number' } },
+            }).input
+          ).toMatchInlineSnapshot(`
+            {
+              "control": {
+                "type": "number"
+              },
+              "name": "input",
+              "type": {
+                "name": "number"
+              }
+            }
+          `);
+        });
+      });
 
-  describe('extraction', () => {
-    it('number', () => {
-      expect(enhance({ extractedArgType: { name: 'input', type: { name: 'number' } } }))
-        .toMatchInlineSnapshot(`
-        {
-          "name": "input",
-          "type": {
-            "name": "number"
-          }
-        }
-      `);
-    });
-  });
+      describe('args input', () => {
+        it('number', () => {
+          expect(enhance({ arg: 5 }).input).toMatchInlineSnapshot(`
+            {
+              "control": {
+                "type": "number"
+              },
+              "name": "input",
+              "type": {
+                "name": "number"
+              }
+            }
+          `);
+        });
+      });
 
-  describe('controls input', () => {
-    it('range', () => {
-      expect(enhance({ argType: { controls: { type: 'range', min: 0, max: 100 } } }))
-        .toMatchInlineSnapshot(`
-        {
-          "name": "input",
-          "controls": {
-            "type": "range",
-            "min": 0,
-            "max": 100
-          }
-        }
-      `);
-    });
-    it('options', () => {
-      expect(
-        enhance({
-          argType: { controls: { type: 'options', options: [1, 2], controlType: 'radio' } },
-        })
-      ).toMatchInlineSnapshot(`
-        {
-          "name": "input",
-          "controls": {
-            "type": "options",
-            "options": [
-              1,
-              2
-            ],
-            "controlType": "radio"
-          }
-        }
-      `);
-    });
-  });
+      describe('extraction from component', () => {
+        it('number', () => {
+          expect(
+            enhance({ extractedArgTypes: { input: { name: 'input', type: { name: 'number' } } } })
+              .input
+          ).toMatchInlineSnapshot(`
+            {
+              "control": {
+                "type": "number"
+              },
+              "name": "input",
+              "type": {
+                "name": "number"
+              }
+            }
+          `);
+        });
+      });
 
-  describe('mixed input', () => {
-    it('mixed input', () => {
-      expect(
-        enhance({
-          argType: { defaultValue: 5, control: { type: 'range', step: 50 } },
-          arg: 3,
-          extractedArgType: { name: 'input' },
-        })
-      ).toMatchInlineSnapshot(`
-        {
-          "name": "input",
-          "defaultValue": 5,
-          "control": {
-            "type": "range",
-            "step": 50
+      describe('controls input', () => {
+        it('range', () => {
+          expect(
+            enhance({
+              argType: { control: { type: 'range', min: 0, max: 100 } },
+            }).input
+          ).toMatchInlineSnapshot(`
+            {
+              "name": "input",
+              "control": {
+                "type": "range",
+                "min": 0,
+                "max": 100
+              }
+            }
+          `);
+        });
+        it('options', () => {
+          expect(
+            enhance({
+              argType: { control: { type: 'options', options: [1, 2], controlType: 'radio' } },
+            }).input
+          ).toMatchInlineSnapshot(`
+            {
+              "name": "input",
+              "control": {
+                "type": "options",
+                "options": [
+                  1,
+                  2
+                ],
+                "controlType": "radio"
+              }
+            }
+          `);
+        });
+      });
+    });
+
+    describe('mixed-source input', () => {
+      it('user-specified argTypes take precedence over extracted argTypes', () => {
+        expect(
+          enhance({
+            argType: { type: { name: 'number' } },
+            extractedArgTypes: { input: { type: { name: 'string' } } },
+          }).input
+        ).toMatchInlineSnapshot(`
+          {
+            "control": {
+              "type": "number"
+            },
+            "type": {
+              "name": "number"
+            },
+            "name": "input"
           }
-        }
-      `);
+        `);
+      });
+
+      it('user-specified argTypes take precedence over inferred argTypes', () => {
+        expect(
+          enhance({
+            argType: { type: { name: 'number' } },
+            arg: 'hello',
+          }).input
+        ).toMatchInlineSnapshot(`
+          {
+            "control": {
+              "type": "number"
+            },
+            "name": "input",
+            "type": {
+              "name": "number"
+            }
+          }
+        `);
+      });
+
+      it('extracted argTypes take precedence over inferred argTypes', () => {
+        expect(
+          enhance({
+            extractedArgTypes: { input: { type: { name: 'string' } } },
+            arg: 6,
+          }).input
+        ).toMatchInlineSnapshot(`
+          {
+            "control": {
+              "type": "text"
+            },
+            "name": "input",
+            "type": {
+              "name": "string"
+            }
+          }
+        `);
+      });
+
+      it('user-specified controls take precedence over inferred controls', () => {
+        expect(
+          enhance({
+            argType: { defaultValue: 5, control: { type: 'range', step: 50 } },
+            arg: 3,
+            extractedArgTypes: { input: { name: 'input' } },
+          }).input
+        ).toMatchInlineSnapshot(`
+          {
+            "control": {
+              "type": "range",
+              "step": 50
+            },
+            "name": "input",
+            "type": {
+              "name": "number"
+            },
+            "defaultValue": 5
+          }
+        `);
+      });
+
+      it('includes extracted argTypes when there are no user-specified argTypes', () => {
+        expect(
+          enhance({
+            arg: 3,
+            extractedArgTypes: { input: { name: 'input' }, foo: { type: { name: 'number' } } },
+          })
+        ).toMatchInlineSnapshot(`
+          {
+            "input": {
+              "control": {
+                "type": "number"
+              },
+              "name": "input",
+              "type": {
+                "name": "number"
+              }
+            },
+            "foo": {
+              "control": {
+                "type": "number"
+              },
+              "type": {
+                "name": "number"
+              }
+            }
+          }
+        `);
+      });
+
+      it('includes extracted argTypes when user-specified argTypes match', () => {
+        expect(
+          enhance({
+            argType: { type: { name: 'number' } },
+            extractedArgTypes: { input: { name: 'input' }, foo: { type: { name: 'number' } } },
+          })
+        ).toMatchInlineSnapshot(`
+          {
+            "input": {
+              "control": {
+                "type": "number"
+              },
+              "name": "input",
+              "type": {
+                "name": "number"
+              }
+            },
+            "foo": {
+              "control": {
+                "type": "number"
+              },
+              "type": {
+                "name": "number"
+              }
+            }
+          }
+        `);
+      });
+
+      it('excludes extracted argTypes when user-specified argTypes do not match', () => {
+        expect(
+          enhance({
+            argType: { type: { name: 'number' } },
+            extractedArgTypes: { foo: { type: { name: 'number' } } },
+          })
+        ).toMatchInlineSnapshot(`
+          {
+            "input": {
+              "control": {
+                "type": "number"
+              },
+              "name": "input",
+              "type": {
+                "name": "number"
+              }
+            }
+          }
+        `);
+      });
     });
   });
 });
--- addons/docs/src/frameworks/common/enhanceArgTypes.ts
+++ addons/docs/src/frameworks/common/enhanceArgTypes.ts
@@ -1,5 +1,5 @@
 import camelCase from 'lodash/camelCase';
-import { ParameterEnhancer, combineParameters } from '@storybook/client-api';
+import { ArgTypesEnhancer, combineParameters } from '@storybook/client-api';
 import { ArgTypes } from '@storybook/api';
 import { inferArgTypes } from './inferArgTypes';
 import { inferControls } from './inferControls';
@@ -11,14 +11,10 @@ const isSubset = (kind: string, subset: object, superset: object) => {
   return overlap.length === keys.length;
 };
 
-export const enhanceArgTypes: ParameterEnhancer = (context) => {
+export const enhanceArgTypes: ArgTypesEnhancer = (context) => {
   const { parameters, args = {} } = context;
   const { component, subcomponents, argTypes = {}, docs = {} } = parameters;
   const { extractArgTypes } = docs;
-  // if (context.storyFn.length === 0 || !component || !extractArgTypes) {
-  if (!component || !extractArgTypes) {
-    return null;
-  }
 
   const namedArgTypes = Object.entries(argTypes as ArgTypes).reduce((acc, [key, val]) => {
     acc[key] = { name: key, ...val };
@@ -27,38 +23,38 @@ export const enhanceArgTypes: ParameterEnhancer = (context) => {
 
   const inferredArgTypes = inferArgTypes(args);
   const components = { Primary: component, ...subcomponents };
-  let extractedArgTypes: ArgTypes;
+  let extractedArgTypes: ArgTypes = {};
 
-  const componentArgTypes = Object.entries(components).reduce((acc, [label, comp]) => {
-    acc[label] = extractArgTypes(comp);
-    return acc;
-  }, {} as Record<string, ArgTypes>);
+  if (extractArgTypes) {
+    const componentArgTypes = Object.entries(components).reduce((acc, [label, comp]) => {
+      acc[label] = extractArgTypes(comp);
+      return acc;
+    }, {} as Record<string, ArgTypes>);
 
-  extractedArgTypes = Object.entries(componentArgTypes).reduce((acc, [label, compTypes]) => {
-    Object.entries(compTypes).forEach(([key, argType]) => {
-      const subLabel = label === 'Primary' ? key : camelCase(`${label} ${key}`);
-      acc[subLabel] = argType;
-    });
-    return acc;
-  }, {} as ArgTypes);
+    extractedArgTypes = Object.entries(componentArgTypes).reduce((acc, [label, compTypes]) => {
+      Object.entries(compTypes).forEach(([key, argType]) => {
+        const subLabel = label === 'Primary' ? key : camelCase(`${label} ${key}`);
+        acc[subLabel] = argType;
+      });
+      return acc;
+    }, {} as ArgTypes);
+  }
 
   if (
-    (Object.keys(argTypes).length > 0 && !isSubset(context.kind, extractedArgTypes, argTypes)) ||
+    (Object.keys(argTypes).length > 0 && !isSubset(context.kind, argTypes, extractedArgTypes)) ||
     (Object.keys(inferredArgTypes).length > 0 &&
-      !isSubset(context.kind, extractedArgTypes, inferredArgTypes))
+      !isSubset(context.kind, inferredArgTypes, extractedArgTypes))
   ) {
     extractedArgTypes = {};
   }
 
-  const withArgTypes = combineParameters(
-    { argTypes: namedArgTypes },
-    { argTypes: inferredArgTypes },
-    { argTypes: extractedArgTypes, componentArgTypes }
-  );
+  const withArgTypes = combineParameters(inferredArgTypes, extractedArgTypes, namedArgTypes);
 
-  const withControls = inferControls(withArgTypes.argTypes);
-
-  const result = combineParameters(withArgTypes, { argTypes: withControls });
+  let result = withArgTypes;
+  if (context.storyFn.length > 0) {
+    const withControls = inferControls(withArgTypes);
+    result = combineParameters(withControls, withArgTypes);
+  }
 
   return result;
 };
