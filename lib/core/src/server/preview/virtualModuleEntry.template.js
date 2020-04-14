zsgovlab/storybook

refactor: use ES imports in the entry virtual module template
We can use latest JS in it as it is now transpiled like all other JS files

lib/core/src/server/preview/virtualModuleEntry.template.js
@@ -1,6 +1,7 @@
-const { addDecorator, addParameters, addParameterEnhancer } = require('{{clientApi}}');
-const { logger } = require('{{clientLogger}}');
-const {
+/* eslint-disable import/no-unresolved */
+import { addDecorator, addParameters, addParameterEnhancer } from '{{clientApi}}';
+import { logger } from '{{clientLogger}}';
+import {
   decorators,
   parameters,
   parameterEnhancers,
@@ -8,7 +9,7 @@ const {
   globalArgTypes,
   args,
   argTypes,
-} = require('{{configFilename}}');
+} from '{{configFilename}}';
 
 if (args || argTypes) {
   logger.warn('Invalid args/argTypes in config, ignoring.', JSON.stringify({ args, argTypes }));

storybookjs/marko

lib/core/src/server/preview/virtualModuleEntry.template.js
@@ -1,6 +1,7 @@
-const { addDecorator, addParameters, addArgTypesEnhancer } = require('{{clientApi}}');
-const { logger } = require('{{clientLogger}}');
-const {
+/* eslint-disable import/no-unresolved */
+import { addDecorator, addParameters, addArgTypesEnhancer } from '{{clientApi}}';
+import { logger } from '{{clientLogger}}';
+import {
   decorators,
   parameters,
   argTypesEnhancers,
@@ -8,7 +9,7 @@ const {
   globalArgTypes,
   args,
   argTypes,
-} = require('{{configFilename}}');
+} from '{{configFilename}}';
 
 if (args || argTypes) {
   logger.warn('Invalid args/argTypes in config, ignoring.', JSON.stringify({ args, argTypes }));