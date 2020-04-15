a4828e44eb670bc6effecaea899c86c3290f7f5d
Michael Shilman <michael@lab80.co>
2020-04-15 22:14:02 +0800

    ArgsTable: Clean up component extraction for primary comonent only

--- addons/docs/src/frameworks/common/enhanceArgTypes.ts
+++ addons/docs/src/frameworks/common/enhanceArgTypes.ts
@@ -12,33 +12,12 @@ const isSubset = (kind: string, subset: object, superset: object) => {
 };
 
 export const enhanceArgTypes: ArgTypesEnhancer = (context) => {
-  const {
-    component,
-    subcomponents,
-    argTypes: userArgTypes = {},
-    docs = {},
-    args = {},
-  } = context.parameters;
+  const { component, argTypes: userArgTypes = {}, docs = {}, args = {} } = context.parameters;
   const { extractArgTypes } = docs;
 
   const namedArgTypes = mapValues(userArgTypes, (val, key) => ({ name: key, ...val }));
   const inferredArgTypes = inferArgTypes(args);
-  const components = { Primary: component, ...subcomponents };
-  let extractedArgTypes: ArgTypes = {};
-
-  if (extractArgTypes && components) {
-    const componentArgTypes = mapValues(components, (comp) => extractArgTypes(comp));
-    extractedArgTypes = Object.entries(componentArgTypes).reduce((acc, [label, compTypes]) => {
-      if (compTypes) {
-        Object.entries(compTypes).forEach(([key, argType]) => {
-          if (label === 'Primary') {
-            acc[key] = argType;
-          }
-        });
-      }
-      return acc;
-    }, {} as ArgTypes);
-  }
+  let extractedArgTypes: ArgTypes = component ? extractArgTypes(component) : {};
 
   if (
     (Object.keys(userArgTypes).length > 0 &&
