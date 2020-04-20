f494187e633111c4cfca2097289a7c0b983382fb
Brandon Chinn <brandon@leapyear.io>
2020-04-20 14:53:38 -0700

    Fix eslint

--- addons/storyshots/storyshots-core/src/frameworks/react/renderTree.ts
+++ addons/storyshots/storyshots-core/src/frameworks/react/renderTree.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import reactTestRenderer from 'react-test-renderer';
 
 function getRenderedTree(story: any, context: any, { renderer, ...rendererOptions }: any) {
