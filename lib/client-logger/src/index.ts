74c1a7c0aad7523494f0c454a5d37c9b74ca6411
Norbert de Langen <ndelangen@me.com>
2020-05-14 17:05:06 +0200

    REMOVE tslint instead
    bye bye

--- lib/client-logger/src/index.ts
+++ lib/client-logger/src/index.ts
@@ -1,7 +1,5 @@
 const { console } = global;
 
-/* tslint:disable: no-console */
-
 export const logger = {
   debug: (message: any, ...rest: any[]): void => console.debug(message, ...rest),
   log: (message: any, ...rest: any[]): void => console.log(message, ...rest),
