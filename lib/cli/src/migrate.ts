d8e77a12e50bbbcb4a1ae9b3761540336292f939
Gaëtan Maisse <gmaisse@solocal.com>
2020-05-14 21:31:56 +0200

    refactor(cli): migrate `migrate` file to TS

--- lib/cli/src/migrate.ts
+++ lib/cli/src/migrate.ts
@@ -1,11 +1,11 @@
 import { listCodemods, runCodemod } from '@storybook/codemod';
 
 export async function migrate(
-  migration,
-  { configDir, glob, dryRun, list, rename, logger, parser }
+  migration: any,
+  { configDir, glob, dryRun, list, rename, logger, parser }: any
 ) {
   if (list) {
-    listCodemods().forEach((key) => logger.log(key));
+    listCodemods().forEach((key: any) => logger.log(key));
   } else if (migration) {
     await runCodemod(migration, { configDir, glob, dryRun, logger, rename, parser });
   } else {
