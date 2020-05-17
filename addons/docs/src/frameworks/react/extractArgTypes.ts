beab55946eb7570474729711e9245b8a2e31d317
Michael Shilman <michael@lab80.co>
2020-05-17 17:42:29 +0800

    ArgTypes: Clean up redundant quote removal

--- addons/docs/src/frameworks/react/extractArgTypes.ts
+++ addons/docs/src/frameworks/react/extractArgTypes.ts
@@ -1,10 +1,7 @@
 import { PropDef, PropsTableRowsProps } from '@storybook/components';
 import { ArgTypes } from '@storybook/api';
 import { ArgTypesExtractor } from '../../lib/docgen';
-import { trimQuotes } from '../../lib/sbtypes/utils';
 import { extractProps } from './extractProps';
-
-const trim = (val: any) => (val && typeof val === 'string' ? trimQuotes(val) : val);
 
 export const extractArgTypes: ArgTypesExtractor = (component) => {
   if (component) {
@@ -13,7 +10,7 @@ export const extractArgTypes: ArgTypesExtractor = (component) => {
     if (rows) {
       return rows.reduce((acc: ArgTypes, row: PropDef) => {
         const { type, sbType, defaultValue: defaultSummary, jsDocTags, required } = row;
-        let defaultValue = defaultSummary && trim(defaultSummary.detail || defaultSummary.summary);
+        let defaultValue = defaultSummary && (defaultSummary.detail || defaultSummary.summary);
         try {
           // eslint-disable-next-line no-eval
           defaultValue = eval(defaultValue);
