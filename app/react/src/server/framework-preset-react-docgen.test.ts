daf193a897631a8964c60d964ca0b89d1f16556b
Michael Shilman <michael@lab80.co>
2020-05-18 14:38:55 +0800

    Update failing tests

--- app/react/src/server/framework-preset-react-docgen.test.ts
+++ app/react/src/server/framework-preset-react-docgen.test.ts
@@ -18,7 +18,7 @@ describe('framework-preset-react-docgen', () => {
       presets: ['env', 'foo-preset'],
       overrides: [
         {
-          test: /\.(mjs|jsx?)$/,
+          test: /\.(mjs|tsx?|jsx?)$/,
           plugins: [
             [
               babelPluginReactDocgenPath,
