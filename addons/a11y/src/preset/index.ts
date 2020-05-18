fafa12350a128be2d74f512251f787bb18c33b78
tooppaaa <clementdungler@gmail.com>
2020-05-18 13:03:53 +0200

    Remove uncessary entry

--- addons/a11y/src/preset/index.ts
+++ addons/a11y/src/preset/index.ts
@@ -1,12 +1,7 @@
 export function managerEntries(entry: any[] = []) {
-  return [...entry, require.resolve('./register')];
+  return [...entry, require.resolve('../register')];
 }
 
-export function config(entry: any[] = [], { addDecorator = true } = {}) {
-  return [
-    ...entry,
-    require.resolve('./a11yRunner'),
-    require.resolve('./a11yHighlight'),
-    addDecorator && require.resolve('./addDecorator'),
-  ].filter(Boolean);
+export function config(entry: any[] = []) {
+  return [...entry, require.resolve('../a11yRunner'), require.resolve('../a11yHighlight')];
 }
