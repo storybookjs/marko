ec7233696e1b646f923ea44d85881bcf12264199
Tom Coleman <tom@thesnail.org>
2020-05-25 17:00:51 +1000

    Update rendering stories with info

--- examples/official-storybook/stories/core/rendering.stories.js
+++ examples/official-storybook/stories/core/rendering.stories.js
@@ -4,6 +4,7 @@ export default {
   title: 'Core/Rendering',
 };
 
+// NOTE: in our example apps each component is mounted twice as we render in strict mode
 let timesMounted = 0;
 export const Counter = () => {
   const countRef = useRef();
