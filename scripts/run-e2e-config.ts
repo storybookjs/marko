6f11a09bbaff3c0dfbff4eb07c6553bc17df1c9f
tooppaaa <clementdungler@gmail.com>
2020-05-25 18:00:46 +0200

    test yarn2

--- scripts/run-e2e-config.ts
+++ scripts/run-e2e-config.ts
@@ -148,7 +148,7 @@ export const yarn2Cra: Parameters = {
   name: 'yarn-2-cra',
   version: 'latest',
   generator: [
-    `yarn set version 2`,
+    `yarn set version latest`,
     `yarn dlx create-react-app@{{version}} {{name}}-v{{version}}`,
     `yarn add core-js -D`, // Temporary fix
   ].join(' && '),
