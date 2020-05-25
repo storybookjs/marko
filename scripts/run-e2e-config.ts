41c1e10399a0ae59bbe69eae0681a2deba29cfcc
tooppaaa <clementdungler@gmail.com>
2020-05-25 19:13:12 +0200

    revert yarn 2 changes

--- scripts/run-e2e-config.ts
+++ scripts/run-e2e-config.ts
@@ -148,8 +148,7 @@ export const yarn2Cra: Parameters = {
   name: 'yarn-2-cra',
   version: 'latest',
   generator: [
-    `yarn set version berry`,
+    `yarn set version 2`,
     `yarn dlx create-react-app@{{version}} {{name}}-v{{version}}`,
-    `yarn add core-js -D`, // Temporary fix
   ].join(' && '),
 };
