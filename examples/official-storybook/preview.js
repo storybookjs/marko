2cb04ea4250223a6acb0707ea6c15277922f6c60
Michael Shilman <michael@lab80.co>
2020-05-26 22:37:54 +0800

    Addon-controls: NOT expanded by default in official-storybook

--- examples/official-storybook/preview.js
+++ examples/official-storybook/preview.js
@@ -52,7 +52,6 @@ addParameters({
       restoreScroll: true,
     },
   },
-  controls: { expanded: true },
   options: {
     storySort: (a, b) =>
       a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
