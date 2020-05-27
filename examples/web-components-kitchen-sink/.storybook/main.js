236da4ef3cdab10a5529b9ac2b28fc8e558ca9cf
Michael Shilman <michael@lab80.co>
2020-05-28 04:22:21 +0800

    Addon-controls: Web-components support

--- examples/web-components-kitchen-sink/.storybook/main.js
+++ examples/web-components-kitchen-sink/.storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   addons: [
     '@storybook/addon-docs',
+    '@storybook/addon-controls',
     '@storybook/addon-a11y',
     '@storybook/addon-actions',
     '@storybook/addon-backgrounds',
