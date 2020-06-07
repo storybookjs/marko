e2864c8471147e6431ae7630e79a2f9fbb0fa22b
Igor <igor-dv@users.noreply.github.com>
2020-06-08 00:49:07 +0300

    Try to fix typings again

--- addons/controls/src/title.ts
+++ addons/controls/src/title.ts
@@ -1,6 +1,6 @@
 import { useArgTypes } from '@storybook/api';
 
-export function getTitle(): () => string {
+export function getTitle(): string {
   const rows = useArgTypes();
 
   const controlsCount = Object.values(rows).filter((argType) => argType?.control?.type).length;
