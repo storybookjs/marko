83e8d04e1d5213c1d57e481b7b95c29056b67d02
Mark Huot <mark@markhuot.com>
2020-06-08 09:19:23 -0400

    removing unused mocks copied over from Angular

--- examples/web-components-kitchen-sink/web-components-storyshots.test.js
+++ examples/web-components-kitchen-sink/web-components-storyshots.test.js
@@ -1,10 +1,6 @@
 import path from 'path';
 import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots';
 
-jest.mock('./addon-jest.testresults.json', () => ({}), { virtual: true });
-jest.mock('./documentation.json', () => ({}), { virtual: true });
-jest.mock('./environments/environment', () => ({}), { virtual: true });
-
 initStoryshots({
   framework: 'web-components',
   integrityOptions: { cwd: path.join(__dirname, 'src', 'stories') },
