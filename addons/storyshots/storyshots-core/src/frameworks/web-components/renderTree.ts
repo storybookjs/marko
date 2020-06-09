1c03a3c4c52fcba9c68661b72d04be013ca52e66
Mark Huot <mark@markhuot.com>
2020-06-09 05:20:45 -0400

    updated to work with lit-html

--- addons/storyshots/storyshots-core/src/frameworks/web-components/renderTree.ts
+++ addons/storyshots/storyshots-core/src/frameworks/web-components/renderTree.ts
@@ -2,16 +2,7 @@ import { document, Node } from 'global';
 
 function getRenderedTree(story: { render: () => any }) {
   const component = story.render();
-  const componentContent = component.getHTML ? component.getHTML() : component;
-
-  const section: HTMLElement = document.createElement('section');
-  section.innerHTML = componentContent;
-
-  if (section.childElementCount > 1) {
-    return section;
-  }
-
-  return section.firstChild;
+  return component.getHTML ? component.getHTML() : component;
 }
 
 export default getRenderedTree;
