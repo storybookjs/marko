30d2279dc808b5a51ccbcec8cda97e1ffc9756bd
Fabien Rassinier <frassinier@talend.com>
2020-06-24 22:59:00 +0200

    fix(addon-docs): list styles over reset

--- lib/components/src/blocks/DocsPage.stories.tsx
+++ lib/components/src/blocks/DocsPage.stories.tsx
@@ -12,8 +12,9 @@ import * as description from './Description.stories';
 export default {
   title: 'Docs/DocsPage',
   component: DocsPageWrapper,
-  // The goal of this decorator is to mimic some CSS reset like Tailwind CSS does
-  // @see https://github.com/tailwindcss/tailwindcss/blob/75e7c10232c95350c0e5fceeb45bd9e57f64b6e9/src/plugins/css/preflight.css#L50
+  // The goal of this decorator is to mimic some CSS reset.
+  // Like Tailwind CSS does, for example.
+  // https://github.com/tailwindcss/tailwindcss/blob/75e7c10232c95350c0e5fceeb45bd9e57f64b6e9/src/plugins/css/preflight.css#L50
   decorators: [
     (storyFn) => (
       <>
