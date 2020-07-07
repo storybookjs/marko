b170597e7553e1c65702a9bc5aae030d283389a2
Tom Coleman <tom@thesnail.org>
2020-07-08 09:41:11 +1000

    Add tests for event handling

--- lib/api/src/tests/events.test.ts
+++ lib/api/src/tests/events.test.ts
@@ -1,14 +1,46 @@
 import { getEventMetadata } from '../lib/events';
 import { API } from '../index';
 
-describe('getEventMetadata', () => {
-  const fullAPI = ({ findRef: jest.fn() } as unknown) as API;
+jest.mock('global', () => ({
+  location: { origin: 'http://localhost:6006', pathname: '/' },
+}));
 
-  it('returns local if the event source is for localhost', () => {
+describe('getEventMetadata', () => {
+  const fullAPIMock = { findRef: jest.fn(), getRefs: jest.fn() };
+  const fullAPI = (fullAPIMock as unknown) as API;
+
+  it('returns local if the event source is the same current location', () => {
     expect(
       getEventMetadata({ type: 'event', source: 'http://localhost:6006' }, fullAPI)
     ).toMatchObject({
       sourceType: 'local',
+    });
+  });
+
+  it('returns external if the refId is set', () => {
+    fullAPIMock.getRefs.mockReset().mockReturnValue({
+      ref: { id: 'ref' },
+    });
+
+    expect(
+      getEventMetadata(
+        { type: 'event', source: 'http://localhost:6006/foo/bar', refId: 'ref' },
+        fullAPI
+      )
+    ).toMatchObject({
+      sourceType: 'external',
+      ref: { id: 'ref' },
+    });
+  });
+
+  it('returns external if the source is set to something other and ref is unset (SB5)', () => {
+    fullAPIMock.findRef.mockReset().mockReturnValue({ id: 'ref' });
+
+    expect(
+      getEventMetadata({ type: 'event', source: 'http://storybook.host' }, fullAPI)
+    ).toMatchObject({
+      sourceType: 'external',
+      ref: { id: 'ref' },
     });
   });
 });
