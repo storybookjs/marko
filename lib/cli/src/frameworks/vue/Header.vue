fba408cccbfd384f1dfc62ff34902eb918163f9f
Michael Shilman <michael@lab80.co>
2020-07-15 15:56:18 +0800

    CLI template: Prettier

--- lib/cli/src/frameworks/vue/Header.vue
+++ lib/cli/src/frameworks/vue/Header.vue
@@ -30,30 +30,30 @@
 </template>
 
 <script>
-import "./header.css";
-import MyButton from "./Button.vue";
+import './header.css';
+import MyButton from './Button.vue';
 
 export default {
-  name: "my-header",
+  name: 'my-header',
 
   components: { MyButton },
 
   props: {
     user: {
-      type: Object
-    }
+      type: Object,
+    },
   },
 
   methods: {
     onLogin() {
-      this.$emit("onLogin");
+      this.$emit('onLogin');
     },
     onLogout() {
-      this.$emit("onLogout");
+      this.$emit('onLogout');
     },
     onCreateAccount() {
-      this.$emit("onCreateAccount");
-    }
-  }
+      this.$emit('onCreateAccount');
+    },
+  },
 };
 </script>
