0d5a6c636cda5c48b2042d07d069ca3ac5208cf4
Mark Berry <markberry867@gmail.com>
2020-07-15 23:22:36 -0500

    Fix lint error

--- app/angular/src/server/angular-cli_utils.ts
+++ app/angular/src/server/angular-cli_utils.ts
@@ -1,5 +1,13 @@
 import fs from 'fs';
-import { basename, dirname, normalize, relative, resolve, Path, getSystemPath } from '@angular-devkit/core';
+import {
+  basename,
+  dirname,
+  normalize,
+  relative,
+  resolve,
+  Path,
+  getSystemPath
+} from '@angular-devkit/core';
 import {
   getCommonConfig,
   getStylesConfig,
