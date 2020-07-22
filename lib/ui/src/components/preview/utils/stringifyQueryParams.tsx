0fc8d477b949ccd07c0bb15d89ccc5adc4d961c9
Akira Sudoh <asudoh@gmail.com>
2020-07-22 19:52:56 +0900

    fix(preview): uri-encode search params
    This change applies basic URI encoding to knob keys/values in
    `<iframe>` URL and torn-away window URL in preview.
    
    Fixes storybookjs#11641.

--- lib/ui/src/components/preview/utils/stringifyQueryParams.tsx
+++ lib/ui/src/components/preview/utils/stringifyQueryParams.tsx
@@ -1,2 +1,5 @@
 export const stringifyQueryParams = (queryParams: Record<string, string>) =>
-  Object.entries(queryParams).reduce((acc, [k, v]) => `${acc}&${k}=${v}`, '');
+  Object.entries(queryParams).reduce(
+    (acc, [k, v]) => `${acc}&${encodeURIComponent(k)}=${encodeURIComponent(v)}`,
+    ''
+  );
