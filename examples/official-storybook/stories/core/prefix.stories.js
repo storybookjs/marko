2dff01ff3bf99f9504312bd31edb510a8ae9bc14
Tom Coleman <tom@thesnail.org>
2020-07-22 15:50:39 +1000

    Update examples/official-storybook/stories/core/prefix.stories.js
    Co-authored-by: Michael Shilman <shilman@users.noreply.github.com>

--- examples/official-storybook/stories/core/prefix.stories.js
+++ examples/official-storybook/stories/core/prefix.stories.js
@@ -1,6 +1,5 @@
 // Very simple stories to show what happens when one story's id is a prefix of anothers'
 // Repro for https://github.com/storybookjs/storybook/issues/11571
-import React from 'react';
 
 export default {
   title: 'Core/Prefix',
