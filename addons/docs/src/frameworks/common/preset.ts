ee45f72e3fbe790fea9eecdf339fbcc750eb82a3
Michael Shilman <michael@lab80.co>
2020-07-28 15:04:31 +0800

    Source-loader: Inject source snippets as story parameters

--- addons/docs/src/frameworks/common/preset.ts
+++ addons/docs/src/frameworks/common/preset.ts
@@ -50,7 +50,7 @@ export function webpack(webpackConfig: any = {}, options: any = {}) {
     babelOptions,
     mdxBabelOptions,
     configureJSX = true,
-    sourceLoaderOptions = {},
+    sourceLoaderOptions = { injectParameters: true },
     transcludeMarkdown = false,
   } = options;
 
