55eb505c2dc973b3dc0e036964204b11f318044a
Kyle Suss <susskyle@gmail.com>
2020-08-01 13:02:36 -0600

    Update TOC format, setup with currently ready pages

--- docs/toc.js
+++ docs/toc.js
@@ -2,56 +2,251 @@ module.exports = {
   toc: [
     {
       title: 'Get Started',
-      prefix: 'get-started',
-      pages: ['introduction', 'install', 'whats-a-story', 'browse-stories', 'setup', 'conclusion'],
+      type: 'menu',
+      children: [
+        {
+          path: '/docs/get-started/introduction/',
+          title: 'Introduction',
+          type: 'bullet-link',
+        },
+        {
+          path: '/docs/get-started/install/',
+          title: 'Install',
+          type: 'bullet-link',
+        },
+        {
+          path: '/docs/get-started/whats-a-story/',
+          title: "What's a story?",
+          type: 'bullet-link',
+        },
+        {
+          path: '/docs/get-started/browse-stories/',
+          title: 'Browse stories',
+          type: 'bullet-link',
+        },
+        {
+          path: '/docs/get-started/setup/',
+          title: 'Setup',
+          type: 'bullet-link',
+        },
+        {
+          path: '/docs/get-started/conclusion/',
+          title: 'Conclusion',
+          type: 'bullet-link',
+        },
+      ],
     },
     {
       title: 'Writing Stories',
-      prefix: 'writing-stories',
-      pages: [
-        'introduction',
-        'args',
-        'parameters',
-        'decorators',
-        'naming-components-and-hierarchy',
+      type: 'menu',
+      children: [
+        {
+          path: '/docs/writing-stories/introduction/',
+          title: 'Introduction',
+          type: 'link',
+        },
+        {
+          path: '/docs/writing-stories/args/',
+          title: 'Args',
+          type: 'link',
+        },
+        {
+          path: '/docs/writing-stories/parameters/',
+          title: 'Parameters',
+          type: 'link',
+        },
+        {
+          path: '/docs/writing-stories/decorators/',
+          title: 'Decorators',
+          type: 'link',
+        },
+        {
+          path: '/docs/writing-stories/naming-components-and-hierarchy/',
+          title: 'Naming components and hierarchy',
+          type: 'link',
+        },
       ],
     },
     {
       title: 'Writing Docs',
-      prefix: 'writing-docs',
-      pages: ['introduction', 'docs-page', 'mdx', 'doc-blocks'],
+      type: 'menu',
+      children: [
+        {
+          path: '/docs/writing-docs/introduction/',
+          title: 'Introduction',
+          type: 'link',
+        },
+        {
+          path: '/docs/writing-docs/docs-page/',
+          title: 'Docs Page',
+          type: 'link',
+        },
+        {
+          path: '/docs/writing-docs/mdx/',
+          title: 'MDX',
+          type: 'link',
+        },
+        {
+          path: '/docs/writing-docs/doc-blocks/',
+          title: 'Docs Blocks',
+          type: 'link',
+        },
+      ],
     },
     {
       title: 'Essentials',
-      prefix:'essentials',
-      pages:['introduction','controls','actions','viewports','backgrounds','toolbars-and-globals']
+      type: 'menu',
+      children: [
+        {
+          path: '/docs/essentials/introduction/',
+          title: 'Introduction',
+          type: 'link',
+        },
+        {
+          path: '/docs/essentials/controls/',
+          title: 'Controls',
+          type: 'link',
+        },
+        {
+          path: '/docs/essentials/actions/',
+          title: 'Actions',
+          type: 'link',
+        },
+        {
+          path: '/docs/essentials/viewports/',
+          title: 'Viewports',
+          type: 'link',
+        },
+        {
+          path: '/docs/essentials/backgrounds/',
+          title: 'Backgrounds',
+          type: 'link',
+        },
+        {
+          path: '/docs/essentials/toolbars-and-globals/',
+          title: 'Toolbars & globals',
+          type: 'link',
+        },
+      ],
     },
     {
       title: 'Configure',
-      prefix: 'configure',
-      pages: ['overview', 'integration', 'story-rendering', 'user-interface'],
+      type: 'menu',
+      children: [
+        {
+          path: '/docs/configure/overview/',
+          title: 'Overview',
+          type: 'link',
+        },
+        {
+          path: '/docs/configure/integration/',
+          title: 'Integration',
+          type: 'link',
+        },
+        {
+          path: '/docs/configure/story-rendering/',
+          title: 'Story rendering',
+          type: 'link',
+        },
+        {
+          path: '/docs/configure/user-interface/',
+          title: 'User interface',
+          type: 'link',
+        },
+      ],
     },
     {
-      title:'Workflows',
-      prefix:'workflows',
-      pages:[
-        'publish-storybook',
-        'build-pages-with-storybook',
-        'stories-for-multiple-components',
-        'testing-with-storybook',
-        'unit-testing',
-        'visual-testing',
-        'interaction-testing',
-        'snapshot-testing',
-        'storybook-composition',
-        'package-composition'
-      ]
+      title: 'Workflows',
+      type: 'menu',
+      children: [
+        {
+          path: '/docs/workflows/publish-storybook/',
+          title: 'Publish Storybook',
+          type: 'link',
+        },
+        {
+          path: '/docs/workflows/build-pages-with-storybook/',
+          title: 'Building pages with Storybook',
+          type: 'link',
+        },
+        {
+          path: '/docs/workflows/stories-for-multiple-components/',
+          title: 'Stories for multiple components',
+          type: 'link',
+        },
+        {
+          title: 'Testing with Storybook',
+          type: 'menu',
+          children: [
+            {
+              path: '/docs/workflows/testing-with-storybook/',
+              title: 'Introduction',
+              type: 'link',
+            },
+            {
+              path: '/docs/workflows/unit-testing/',
+              title: 'Unit testing',
+              type: 'link',
+            },
+            {
+              path: '/docs/workflows/visual-testing/',
+              title: 'Visual testing',
+              type: 'link',
+            },
+            {
+              path: '/docs/workflows/interaction-testing/',
+              title: 'Interaction testing',
+              type: 'link',
+            },
+            {
+              path: '/docs/workflows/snapshot-testing/',
+              title: 'Snapshot testing',
+              type: 'link',
+            },
+          ],
+        },
+        {
+          path: '/docs/workflows/storybook-composition/',
+          title: 'Storybook Composition',
+          type: 'link',
+        },
+        {
+          path: '/docs/workflows/package-composition/',
+          title: 'Package Composition',
+          type: 'link',
+        },
+      ],
     },
     {
-
       title: 'API',
-      prefix:'api',
-      pages:['stories','addons','new-frameworks','cli-options','frameworks-feature-support']
+      type: 'menu',
+      children: [
+        {
+          path: '/docs/api/stories/',
+          title: 'Stories',
+          type: 'link',
+        },
+        {
+          path: '/docs/api/addons/',
+          title: 'Addons',
+          type: 'link',
+        },
+        {
+          path: '/docs/api/new-frameworks/',
+          title: 'Frameworks',
+          type: 'link',
+        },
+        {
+          path: '/docs/api/cli-options/',
+          title: 'CLI Options',
+          type: 'link',
+        },
+        {
+          path: '/docs/api/frameworks-feature-support/',
+          title: 'Feature support for frameworks',
+          type: 'link',
+        },
+      ],
     },
   ],
 };
