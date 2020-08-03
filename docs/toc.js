8cd311d4d73ee4d80f6f22e61f6b767beca739d3
Kyle Suss <susskyle@gmail.com>
2020-08-03 17:06:32 -0600

    Update logic to just use pathSegments vs full paths

--- docs/toc.js
+++ docs/toc.js
@@ -2,66 +2,79 @@ module.exports = {
   toc: [
     {
       title: 'Get Started',
+      pathSegment: 'get-started',
       type: 'menu',
       children: [
         {
-          path: '/docs/get-started/introduction/',
+          pathSegment: 'introduction',
           title: 'Introduction',
           type: 'bullet-link',
         },
         {
-          path: '/docs/get-started/install/',
+          pathSegment: 'install',
           title: 'Install',
           type: 'bullet-link',
+          // DOCSTODO: Update the description
+          description: 'Install the Storybook package in your project',
         },
         {
-          path: '/docs/get-started/whats-a-story/',
+          pathSegment: 'whats-a-story',
           title: "What's a story?",
           type: 'bullet-link',
+          // DOCSTODO: Update the description
+          description: 'Learn the base construct of stories within Storybook',
         },
         {
-          path: '/docs/get-started/browse-stories/',
+          pathSegment: 'browse-stories',
           title: 'Browse stories',
           type: 'bullet-link',
+          // DOCSTODO: Update the description
+          description: 'Learn how to explore your stories within Storybook',
         },
         {
-          path: '/docs/get-started/setup/',
+          pathSegment: 'setup',
           title: 'Setup',
           type: 'bullet-link',
+          // DOCSTODO: Update the description
+          description:
+            'Write your first story & adjust Storybook configuration for your environment',
         },
         {
-          path: '/docs/get-started/conclusion/',
+          pathSegment: 'conclusion',
           title: 'Conclusion',
           type: 'bullet-link',
+          // DOCSTODO: Update the description
+          description: 'Take your Storybook skills to the next level',
         },
       ],
     },
     {
       title: 'Writing Stories',
+      pathSegment: 'writing-stories',
       type: 'menu',
       children: [
         {
-          path: '/docs/writing-stories/introduction/',
+          pathSegment: 'introduction',
           title: 'Introduction',
           type: 'link',
         },
         {
-          path: '/docs/writing-stories/args/',
+          pathSegment: 'args',
           title: 'Args',
           type: 'link',
         },
         {
-          path: '/docs/writing-stories/parameters/',
+          pathSegment: 'parameters',
           title: 'Parameters',
           type: 'link',
         },
         {
-          path: '/docs/writing-stories/decorators/',
+          pathSegment: 'decorators',
           title: 'Decorators',
           type: 'link',
         },
         {
-          path: '/docs/writing-stories/naming-components-and-hierarchy/',
+          pathSegment: 'naming-components-and-hierarchy',
           title: 'Naming components and hierarchy',
           type: 'link',
         },
@@ -69,25 +82,26 @@ module.exports = {
     },
     {
       title: 'Writing Docs',
+      pathSegment: 'writing-docs',
       type: 'menu',
       children: [
         {
-          path: '/docs/writing-docs/introduction/',
+          pathSegment: 'introduction',
           title: 'Introduction',
           type: 'link',
         },
         {
-          path: '/docs/writing-docs/docs-page/',
+          pathSegment: 'docs-page',
           title: 'Docs Page',
           type: 'link',
         },
         {
-          path: '/docs/writing-docs/mdx/',
+          pathSegment: 'mdx',
           title: 'MDX',
           type: 'link',
         },
         {
-          path: '/docs/writing-docs/doc-blocks/',
+          pathSegment: 'doc-blocks',
           title: 'Docs Blocks',
           type: 'link',
         },
@@ -95,35 +109,36 @@ module.exports = {
     },
     {
       title: 'Essentials',
+      pathSegment: 'essentials',
       type: 'menu',
       children: [
         {
-          path: '/docs/essentials/introduction/',
+          pathSegment: 'introduction',
           title: 'Introduction',
           type: 'link',
         },
         {
-          path: '/docs/essentials/controls/',
+          pathSegment: 'controls',
           title: 'Controls',
           type: 'link',
         },
         {
-          path: '/docs/essentials/actions/',
+          pathSegment: 'actions',
           title: 'Actions',
           type: 'link',
         },
         {
-          path: '/docs/essentials/viewports/',
+          pathSegment: 'viewports',
           title: 'Viewports',
           type: 'link',
         },
         {
-          path: '/docs/essentials/backgrounds/',
+          pathSegment: 'backgrounds',
           title: 'Backgrounds',
           type: 'link',
         },
         {
-          path: '/docs/essentials/toolbars-and-globals/',
+          pathSegment: 'toolbars-and-globals',
           title: 'Toolbars & globals',
           type: 'link',
         },
@@ -131,25 +146,26 @@ module.exports = {
     },
     {
       title: 'Configure',
+      pathSegment: 'configure',
       type: 'menu',
       children: [
         {
-          path: '/docs/configure/overview/',
+          pathSegment: 'overview',
           title: 'Overview',
           type: 'link',
         },
         {
-          path: '/docs/configure/integration/',
+          pathSegment: 'integration',
           title: 'Integration',
           type: 'link',
         },
         {
-          path: '/docs/configure/story-rendering/',
+          pathSegment: 'story-rendering',
           title: 'Story rendering',
           type: 'link',
         },
         {
-          path: '/docs/configure/user-interface/',
+          pathSegment: 'user-interface',
           title: 'User interface',
           type: 'link',
         },
@@ -157,61 +173,64 @@ module.exports = {
     },
     {
       title: 'Workflows',
+      pathSegment: 'workflows',
       type: 'menu',
       children: [
         {
-          path: '/docs/workflows/publish-storybook/',
+          pathSegment: 'publish-storybook',
           title: 'Publish Storybook',
           type: 'link',
         },
         {
-          path: '/docs/workflows/build-pages-with-storybook/',
+          pathSegment: 'build-pages-with-storybook',
           title: 'Building pages with Storybook',
           type: 'link',
         },
         {
-          path: '/docs/workflows/stories-for-multiple-components/',
+          pathSegment: 'stories-for-multiple-components',
           title: 'Stories for multiple components',
           type: 'link',
         },
         {
           title: 'Testing with Storybook',
+          // Despite having a child menu, this does not currently affect the path
+          pathSegment: '',
           type: 'menu',
           children: [
             {
-              path: '/docs/workflows/testing-with-storybook/',
+              pathSegment: 'testing-with-storybook',
               title: 'Introduction',
               type: 'link',
             },
             {
-              path: '/docs/workflows/unit-testing/',
+              pathSegment: 'unit-testing',
               title: 'Unit testing',
               type: 'link',
             },
             {
-              path: '/docs/workflows/visual-testing/',
+              pathSegment: 'visual-testing',
               title: 'Visual testing',
               type: 'link',
             },
             {
-              path: '/docs/workflows/interaction-testing/',
+              pathSegment: 'interaction-testing',
               title: 'Interaction testing',
               type: 'link',
             },
             {
-              path: '/docs/workflows/snapshot-testing/',
+              pathSegment: 'snapshot-testing',
               title: 'Snapshot testing',
               type: 'link',
             },
           ],
         },
         {
-          path: '/docs/workflows/storybook-composition/',
+          pathSegment: 'storybook-composition',
           title: 'Storybook Composition',
           type: 'link',
         },
         {
-          path: '/docs/workflows/package-composition/',
+          pathSegment: 'package-composition',
           title: 'Package Composition',
           type: 'link',
         },
@@ -219,30 +238,31 @@ module.exports = {
     },
     {
       title: 'API',
+      pathSegment: 'api',
       type: 'menu',
       children: [
         {
-          path: '/docs/api/stories/',
+          pathSegment: 'stories',
           title: 'Stories',
           type: 'link',
         },
         {
-          path: '/docs/api/addons/',
+          pathSegment: 'addons',
           title: 'Addons',
           type: 'link',
         },
         {
-          path: '/docs/api/new-frameworks/',
+          pathSegment: 'new-frameworks',
           title: 'Frameworks',
           type: 'link',
         },
         {
-          path: '/docs/api/cli-options/',
+          pathSegment: 'cli-options',
           title: 'CLI Options',
           type: 'link',
         },
         {
-          path: '/docs/api/frameworks-feature-support/',
+          pathSegment: 'frameworks-feature-support',
           title: 'Feature support for frameworks',
           type: 'link',
         },
