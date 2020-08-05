23063a6fdf0f8c50c598e4021ae862f0c753540d
domyen <dom@hichroma.com>
2020-08-04 20:38:45 -0400

    fix theming bug with NoControlsWarning

--- lib/components/src/blocks/ArgsTable/NoControlsWarning.tsx
+++ lib/components/src/blocks/ArgsTable/NoControlsWarning.tsx
@@ -4,6 +4,7 @@ import { Link } from '../../typography/link/link';
 
 const NoControlsWrapper = styled.div(({ theme }) => ({
   background: theme.background.warning,
+  color: theme.color.darkest,
   padding: '10px 15px',
   lineHeight: '20px',
   boxShadow: `${theme.appBorderColor} 0 -1px 0 0 inset`,
