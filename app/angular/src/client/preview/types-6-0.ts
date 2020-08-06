73407126fc2b5229d50cb146b8dc5023922d4f59
tooppaaa <clementdungler@gmail.com>
2020-08-06 18:59:28 +0200

    cli: apply types for angular

---
+++ app/angular/src/client/preview/types-6-0.ts
@@ -0,0 +1,23 @@
+import { Args as DefaultArgs, Annotations, BaseMeta, BaseStory } from '@storybook/addons';
+import { StoryFnAngularReturnType } from './types';
+
+export { Args, ArgTypes, Parameters, StoryContext } from '@storybook/addons';
+
+type AngularComponent = any;
+type AngularReturnType = StoryFnAngularReturnType;
+
+/**
+ * Metadata to configure the stories for a component.
+ *
+ * @see [Default export](https://storybook.js.org/docs/formats/component-story-format/#default-export)
+ */
+export type Meta<Args = DefaultArgs> = BaseMeta<AngularComponent> &
+  Annotations<Args, AngularReturnType>;
+
+/**
+ * Story function that represents a component example.
+ *
+ * @see [Named Story exports](https://storybook.js.org/docs/formats/component-story-format/#named-story-exports)
+ */
+export type Story<Args = DefaultArgs> = BaseStory<Args, AngularReturnType> &
+  Annotations<Args, AngularReturnType>;
