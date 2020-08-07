f9c782542509c15ab374ce2f255e7c3f181af80a
Michael Shilman <michael@lab80.co>
2020-08-07 11:51:35 +0800

    Upgrade react-popper-tooltip to 3.1.0

--- lib/components/src/tooltip/WithTooltip.tsx
+++ lib/components/src/tooltip/WithTooltip.tsx
@@ -3,7 +3,7 @@ import { styled } from '@storybook/theming';
 import { document } from 'global';
 
 import TooltipTrigger from 'react-popper-tooltip';
-import { Modifiers, Placement } from 'popper.js';
+import { Modifier, Placement } from '@popperjs/core';
 import { Tooltip } from './Tooltip';
 
 // A target that doesn't speak popper
@@ -25,7 +25,7 @@ export interface WithTooltipPureProps {
   trigger?: 'none' | 'hover' | 'click' | 'right-click';
   closeOnClick?: boolean;
   placement?: Placement;
-  modifiers?: Modifiers;
+  modifiers?: Array<Modifier<any, any>>;
   hasChrome?: boolean;
   tooltip: ReactNode | ((p: WithHideFn) => ReactNode);
   children: ReactNode;
@@ -93,7 +93,7 @@ WithTooltipPure.defaultProps = {
   trigger: 'hover',
   closeOnClick: false,
   placement: 'top',
-  modifiers: {},
+  modifiers: [],
   hasChrome: true,
   tooltipShown: false,
 };
