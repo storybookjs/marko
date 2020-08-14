c091c6f1a00093c7190f5bd37a3930e28cca4fe2
Michael Shilman <michael@lab80.co>
2020-08-14 11:15:31 +0800

    Fix linting

--- examples/angular-cli/.storybook/preview.ts
+++ examples/angular-cli/.storybook/preview.ts
@@ -3,6 +3,7 @@ import { setCompodocJson } from '@storybook/addon-docs/angular';
 import addCssWarning from '../src/cssWarning';
 
 // @ts-ignore
+// eslint-disable-next-line import/extensions, import/no-unresolved
 import docJson from '../documentation.json';
 // remove ButtonComponent to test #12009
 const filtered = !docJson?.components
