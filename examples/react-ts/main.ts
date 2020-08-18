2ecbd85cf40b30dcbbb7b342e29e0060969676c1
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-08-18 09:01:16 -0700

    chore: add 'as Meta' use case to react-ts example

--- examples/react-ts/main.ts
+++ examples/react-ts/main.ts
@@ -3,7 +3,20 @@ import type { StorybookConfig } from '@storybook/core/types';
 module.exports = {
   stories: ['./src/*.stories.*'],
   logLevel: 'debug',
-  addons: ['@storybook/addon-essentials', '@storybook/addon-controls'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls',
+    '@storybook/addon-storysource',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        sourceLoaderOptions: {
+          parser: 'typescript',
+          injectStoryParameters: false,
+        },
+      },
+    },
+  ],
   typescript: {
     check: true,
     checkOptions: {},
