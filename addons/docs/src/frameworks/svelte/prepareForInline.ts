9984813928574bbf608316ef6263471bcf8d2d2f
blackfenix2 <devin_98@msn.com>
2020-08-29 03:01:42 -0400

    made a .svelte component load inline

--- addons/docs/src/frameworks/svelte/prepareForInline.ts
+++ addons/docs/src/frameworks/svelte/prepareForInline.ts
@@ -3,6 +3,13 @@ import { StoryFn, StoryContext } from '@storybook/addons';
 // @ts-ignore
 import toReact from 'svelte-adapter/react';
 
+// @ts-ignore
+import sveltedoc from 'sveltedoc-parser';
+import React from 'react';
+
+// @ts-ignore
+import HOC from './HOC.svelte';
+
 // Inspired by https://github.com/egoist/vue-to-react,
 // modified to store args as props in the root store
 
@@ -11,5 +18,19 @@ const COMPONENT = 'STORYBOOK_COMPONENT';
 const VALUES = 'STORYBOOK_VALUES';
 
 export const prepareForInline = (storyFn: StoryFn, { args }: StoryContext) => {
-  return toReact(storyFn());
+  const component = toReact(storyFn());
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const root = new HOC({
+      target: el.current,
+      props: {
+        [COMPONENT]: component,
+        [VALUES]: args,
+      },
+    });
+    console.log(component);
+    return () => root.$destroy();
+  });
+  return React.createElement('div', null, React.createElement('div', { ref: el }));
 };
--- addons/docs/src/frameworks/svelte/preset.ts
+++ addons/docs/src/frameworks/svelte/preset.ts
@@ -2,7 +2,7 @@ import autoPreprocess from 'svelte-preprocess';
 
 export function webpackFinal(webpackConfig: any = {}, options: any = {}) {
   const svelteLoader = webpackConfig.module.rules.find(
-    (r: { loader: string | string[] }) => r.loader && r.loader.includes('ass')
+    (r: { loader: string | string[] }) => r.loader && r.loader.includes('svelte-loader')
   );
   svelteLoader.options.preprocess = autoPreprocess();
   webpackConfig.resolve.extensions.push('.ts', '.tsx');
