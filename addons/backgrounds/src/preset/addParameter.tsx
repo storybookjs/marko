f57a2be0a205b7ab4dd6e50bb61626091a84c429
Yann Braga <yannbf@gmail.com>
2020-09-03 18:44:28 +0200

    feat(addon-backgrounds): make grid available in canvas and docs
    - bring logic into a decorator so it's also compatible with docs

--- addons/backgrounds/src/preset/addParameter.tsx
+++ addons/backgrounds/src/preset/addParameter.tsx
@@ -1,5 +1,6 @@
 export const parameters = {
   backgrounds: {
+    gridSize: 20,
     values: [
       { name: 'light', value: '#F8F8F8' },
       { name: 'dark', value: '#333333' },
--- addons/backgrounds/src/register.tsx
+++ addons/backgrounds/src/register.tsx
@@ -9,7 +9,7 @@ addons.register(ADDON_ID, () => {
   addons.add(ADDON_ID, {
     title: 'Backgrounds',
     type: types.TOOL,
-    match: ({ viewMode }) => viewMode === 'story',
+    match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
     render: () => (
       <Fragment>
         <BackgroundSelector />
