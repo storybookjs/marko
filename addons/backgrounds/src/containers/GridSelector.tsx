47ff571b756d9aaa413f84dc3c9e44773d928964
Yann Braga <yannbf@gmail.com>
2020-09-03 11:21:25 +0200

    fix(grid-selector): apply grid to the preview body
    - Previously it was applied to the iframe, and sometimes it became unusable
    - given that in a few scenarios the background of the body had more specificity.

--- addons/backgrounds/src/containers/GridSelector.tsx
+++ addons/backgrounds/src/containers/GridSelector.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent, memo } from 'react';
+import { document } from 'global';
+import React, { FunctionComponent, memo, useMemo, useEffect } from 'react';
 
 import { useAddonState, useParameter } from '@storybook/api';
-import { Global } from '@storybook/theming';
 import { Icons, IconButton } from '@storybook/components';
+import { logger } from '@storybook/client-logger';
 
 import { ADDON_ID, GRID_PARAM_KEY } from '../constants';
 
@@ -10,42 +11,54 @@ export interface BackgroundGridParameters {
   cellSize: number;
 }
 
-const iframeId = 'storybook-preview-iframe';
-
 export const GridSelector: FunctionComponent = memo(() => {
-  const [state, setState] = useAddonState<boolean>(`${ADDON_ID}/grid`);
+  const [isActive, setIsActive] = useAddonState<boolean>(`${ADDON_ID}/grid`);
   const { cellSize } = useParameter<BackgroundGridParameters>(GRID_PARAM_KEY, { cellSize: 20 });
+
+  const gridStyles = useMemo(
+    () => ({
+      backgroundSize: [
+        `${cellSize * 5}px ${cellSize * 5}px`,
+        `${cellSize * 5}px ${cellSize * 5}px`,
+        `${cellSize}px ${cellSize}px`,
+        `${cellSize}px ${cellSize}px`,
+      ].join(', '),
+      backgroundPosition: '-1px -1px, -1px -1px, -1px -1px, -1px -1px',
+      backgroundBlendMode: 'difference',
+      backgroundImage: [
+        'linear-gradient(rgba(130, 130, 130, 0.5) 1px, transparent 1px)',
+        'linear-gradient(90deg, rgba(130, 130, 130, 0.5) 1px, transparent 1px)',
+        'linear-gradient(rgba(130, 130, 130, 0.25) 1px, transparent 1px)',
+        'linear-gradient(90deg, rgba(130, 130, 130, 0.25) 1px, transparent 1px)',
+      ].join(', '),
+    }),
+    [cellSize]
+  );
+
+  useEffect(() => {
+    const preview = document.querySelector('#storybook-preview-iframe')?.contentDocument
+      ?.body as HTMLElement;
+    if (preview) {
+      Object.keys(gridStyles).forEach((styleKey: string) => {
+        // @ts-ignore ignoring this unfortunately seems to be the only way
+        preview.style[styleKey] = isActive ? gridStyles[styleKey] : 'initial';
+      });
+    } else {
+      logger.warn(`
+        Backgrounds Addon: could not find the preview element to apply the grid on. 
+        Please file an issue on github: https://github.com/storybookjs/storybook/issues/new/
+      `);
+    }
+  }, [isActive]);
 
   return (
     <IconButton
       key="background"
-      active={state}
+      active={isActive}
       title="Change the background of the preview"
-      onClick={() => setState(!state)}
+      onClick={() => setIsActive(!isActive)}
     >
       <Icons icon="grid" />
-      {state ? (
-        <Global
-          styles={{
-            [`#${iframeId}`]: {
-              backgroundSize: [
-                `${cellSize * 5}px ${cellSize * 5}px`,
-                `${cellSize * 5}px ${cellSize * 5}px`,
-                `${cellSize}px ${cellSize}px`,
-                `${cellSize}px ${cellSize}px`,
-              ].join(', '),
-              backgroundPosition: '-1px -1px, -1px -1px, -1px -1px, -1px -1px',
-              backgroundBlendMode: 'difference',
-              backgroundImage: [
-                'linear-gradient(rgba(130, 130, 130, 0.5) 1px, transparent 1px)',
-                'linear-gradient(90deg, rgba(130, 130, 130, 0.5) 1px, transparent 1px)',
-                'linear-gradient(rgba(130, 130, 130, 0.25) 1px, transparent 1px)',
-                'linear-gradient(90deg, rgba(130, 130, 130, 0.25) 1px, transparent 1px)',
-              ].join(', '),
-            },
-          }}
-        />
-      ) : null}
     </IconButton>
   );
 });
