c27ba990d855c7c3edc40e99800578faf03b5986
Clément DUNGLER <clementdungler@gmail.com>
2020-09-07 22:05:08 +0200

    remove duplicate types

--- app/web-components/src/client/preview/types.ts
+++ app/web-components/src/client/preview/types.ts
@@ -21,18 +21,3 @@ export interface ShowErrorArgs {
   title: string;
   description: string;
 }
-
-/**
- * Metadata to configure the stories for a component.
- *
- * @see [Default export](https://storybook.js.org/docs/formats/component-story-format/#default-export)
- */
-export type Meta<Args = DefaultArgs> = BaseMeta<string> & Annotations<Args, StoryFnHtmlReturnType>;
-
-/**
- * Story function that represents a component example.
- *
- * @see [Named Story exports](https://storybook.js.org/docs/formats/component-story-format/#named-story-exports)
- */
-export type Story<Args = DefaultArgs> = BaseStory<Args, StoryFnHtmlReturnType> &
-  Annotations<Args, StoryFnHtmlReturnType>;
