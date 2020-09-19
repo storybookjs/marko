13e5e183ee254c01376ebb641ca1a3eedde33880
Clément DUNGLER <clementdungler@gmail.com>
2020-09-19 14:59:59 +0200

    cleanup and doc

--- app/react/src/server/types.ts
+++ app/react/src/server/types.ts
@@ -1,5 +1,8 @@
 import { StorybookOptions as BaseOptions } from '@storybook/core/types';
 
+/**
+ * The internal options object, used by Storybook frameworks and addons.
+ */
 export interface StorybookOptions extends BaseOptions {
   reactOptions?: {
     fastRefresh?: boolean;
--- app/react/types/index.ts
+++ app/react/types/index.ts
@@ -1,5 +1,8 @@
 import { StorybookConfig as BaseConfig } from '@storybook/core/types';
 
+/**
+ * The interface for Storybook configuration in `main.ts` files.
+ */
 export interface StorybookConfig extends BaseConfig {
   reactOptions?: {
     fastRefresh?: boolean;
