fe4663436522f387e9687cc6a80097865017642b
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-10-09 00:39:48 +0100

    fix for the queryparams addon placement

--- docs/frameworks.js
+++ docs/frameworks.js
@@ -72,7 +72,7 @@ module.exports = {
           unsupported: ['marko'],
         },
         {
-          name: 'query-params',
+          name: 'queryparams',
           unsupported: [],
         },
         {
