9f173d6a70270d3e1c350e2f254888bbc9ccfcb9
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-10-16 23:17:30 +0530

    refactor: leverage arg-parser lib to capture unrecognized args

--- lib/cli/src/generate.ts
+++ lib/cli/src/generate.ts
@@ -89,12 +89,11 @@ program
     })
   );
 
-program.command('*', { noHelp: true }).action(() => {
-  const [, , invalidCmd] = process.argv;
-  logger.error(' Invalid command: %s.\n See --help for a list of available commands.', invalidCmd);
+program.on('command:*', (args) => {
+  logger.error(' Invalid command: %s.\n See --help for a list of available commands.', args[0]);
   // eslint-disable-next-line
   const availableCommands = program.commands.map((cmd) => cmd._name);
-  const suggestion = availableCommands.find((cmd) => leven(cmd, invalidCmd) < 3);
+  const suggestion = availableCommands.find((cmd) => leven(cmd, args[0]) < 3);
   if (suggestion) {
     logger.log(`\n Did you mean ${suggestion}?`);
   }
