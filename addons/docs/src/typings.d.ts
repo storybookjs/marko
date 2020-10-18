537213926e890872fbfe2755daa534f343eb15fd
Shota Fuji <pockawoooh@gmail.com>
2020-10-18 13:38:57 +0000

    feat(addon-docs): Dynamic source rendering for Vue
    https://github.com/storybookjs/storybook/issues/11400
    
    This commit adds dynamic source code rendering feature to Docs addon for
    Vue.js. The goals are 1) reflecting Controls' value to code block and
    2) showing a code similar to what component consumers would write.
    
    To archive these goals, this feature compiles a component with Vue, then
    walks through vdom and stringifys the result. It could be possible to
    parse components' template or render function then stringify results,
    but it would be costly and hard to maintain (especially for parsing).
    We can use vue-template-compiler for the purpose, but it can't handle
    render functions so it's not the way to go IMO.
    
    Speaking of the goal 2, someone wants events to be in the output code.
    But it's so hard to retrieve component definitions (e.g. `methods`,
    `computed`). I think it's okay to skip events until we figure there is a
    high demand for that.

--- addons/docs/src/typings.d.ts
+++ addons/docs/src/typings.d.ts
@@ -7,6 +7,7 @@ declare module 'babel-plugin-react-docgen';
 declare module 'require-from-string';
 declare module 'styled-components';
 declare module 'acorn-jsx';
+declare module 'vue/dist/vue';
 
 declare module 'sveltedoc-parser' {
   export function parse(options: any): Promise<any>;
