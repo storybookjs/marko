87149e839b4f95fb258de027852cae67a34f7bb1
Cameron Yick <cameron.yick@datadoghq.com>
2020-10-19 23:35:47 -0400

    (feat:controls) Update documentation examples for object controls to show when object props are updating

--- examples/official-storybook/stories/addon-controls.stories.tsx
+++ examples/official-storybook/stories/addon-controls.stories.tsx
@@ -11,22 +11,35 @@ export default {
   },
 };
 
-const Template = (args) => <Button {...args} />;
+const DEFAULT_NESTED_OBJECT = { a: 4, b: { c: 'hello', d: [1, 2, 3] } };
+
+const Template = (args) => (
+  <div>
+    <Button type={args.type}>{args.children}</Button>
+    {args.somethingElse && JSON.stringify(args.somethingElse)}
+  </div>
+);
 
 export const Basic = Template.bind({});
 Basic.args = {
   children: 'basic',
-  somethingElse: { a: 2 },
+  somethingElse: DEFAULT_NESTED_OBJECT,
 };
 
 export const Action = Template.bind({});
 Action.args = {
   children: 'hmmm',
   type: 'action',
-  somethingElse: { a: 4 },
+  somethingElse: DEFAULT_NESTED_OBJECT,
 };
 
 export const CustomControls = Template.bind({});
+CustomControls.args = {
+  children: 'hmmm',
+  type: 'action',
+  somethingElse: DEFAULT_NESTED_OBJECT,
+};
+
 CustomControls.argTypes = {
   children: { table: { disable: true } },
   type: { control: { disable: true } },
