231b6aef1388c15046a09e0dd7e1e75fe122b8f9
Michael Shilman <shilman@users.noreply.github.com>
2020-10-23 14:20:42 +0800

    Merge pull request #12873 from storybookjs/12872-essentials-absolute-config
    Essentials: Fix absolute config dir

--- addons/essentials/src/index.ts
+++ addons/essentials/src/index.ts
@@ -10,7 +10,10 @@ interface PresetOptions {
 
 const requireMain = (configDir: string) => {
   let main = {};
-  const mainFile = path.join(process.cwd(), configDir, 'main');
+  const absoluteConfigDir = path.isAbsolute(configDir)
+    ? configDir
+    : path.join(process.cwd(), configDir);
+  const mainFile = path.join(absoluteConfigDir, 'main');
   try {
     // eslint-disable-next-line global-require,import/no-dynamic-require
     main = require(mainFile);
