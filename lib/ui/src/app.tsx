ec07da9a403edf9f87395d610873e291ec9386b6
Gert Hengeveld <info@ghengeveld.nl>
2020-11-16 16:43:58 +0100

    Stick with the sizeMe HOC and use props comparison to avoid rerenders instead.

--- lib/ui/src/app.tsx
+++ lib/ui/src/app.tsx
@@ -1,6 +1,6 @@
-import React, { ComponentProps, FunctionComponent, useMemo } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import { Global, createGlobal, styled } from '@storybook/theming';
-import { SizeMe } from 'react-sizeme';
+import sizeMe from 'react-sizeme';
 
 import { Route } from '@storybook/router';
 
@@ -26,71 +26,90 @@ export interface AppProps {
   docsOnly: boolean;
   layout: State['layout'];
   panelCount: number;
-  width: number;
-  height: number;
+  size: {
+    width: number;
+    height: number;
+  };
 }
 
-const App = React.memo<AppProps>(({ viewMode, docsOnly, layout, panelCount, width, height }) => {
-  let content;
+const App = React.memo<AppProps>(
+  ({ viewMode, docsOnly, layout, panelCount, size: { width, height } }) => {
+    let content;
 
-  const props = useMemo(
-    () => ({
-      Sidebar,
-      Preview,
-      Panel,
-      Notifications,
-      pages: [
-        {
-          key: 'settings',
-          render: () => <SettingsPages />,
-          route: (({ children }) => (
-            <Route path="/settings" startsWith>
-              {children}
-            </Route>
-          )) as FunctionComponent,
-        },
-      ],
-    }),
-    []
-  );
-
-  if (!width || !height) {
-    content = <div />;
-  } else if (width < 600) {
-    content = <Mobile {...props} viewMode={viewMode} options={layout} />;
-  } else {
-    content = (
-      <Desktop
-        {...props}
-        viewMode={viewMode}
-        options={layout}
-        docsOnly={docsOnly}
-        width={width}
-        height={height}
-        panelCount={panelCount}
-      />
+    const props = useMemo(
+      () => ({
+        Sidebar,
+        Preview,
+        Panel,
+        Notifications,
+        pages: [
+          {
+            key: 'settings',
+            render: () => <SettingsPages />,
+            route: (({ children }) => (
+              <Route path="/settings" startsWith>
+                {children}
+              </Route>
+            )) as FunctionComponent,
+          },
+        ],
+      }),
+      []
     );
-  }
 
-  return (
-    <View>
-      <Global styles={createGlobal} />
-      {content}
-    </View>
-  );
-});
+    if (!width || !height) {
+      content = <div />;
+    } else if (width < 600) {
+      content = <Mobile {...props} viewMode={viewMode} options={layout} />;
+    } else {
+      content = (
+        <Desktop
+          {...props}
+          viewMode={viewMode}
+          options={layout}
+          docsOnly={docsOnly}
+          {...{ width, height }}
+          panelCount={panelCount}
+        />
+      );
+    }
+
+    return (
+      <View>
+        <Global styles={createGlobal} />
+        {content}
+      </View>
+    );
+  },
+  // This is the default shallowEqual implementation, but with custom behavior for the `size` prop.
+  (prevProps: any, nextProps: any) => {
+    if (Object.is(prevProps, nextProps)) return true;
+    if (typeof prevProps !== 'object' || prevProps === null) return false;
+    if (typeof nextProps !== 'object' || nextProps === null) return false;
+
+    const keysA = Object.keys(prevProps);
+    const keysB = Object.keys(nextProps);
+    if (keysA.length !== keysB.length) return false;
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key of keysA) {
+      if (key === 'size') {
+        // SizeMe injects a new `size` object every time, even if the width/height doesn't change,
+        // so we chech that one manually.
+        if (prevProps[key].width !== nextProps[key].width) return false;
+        if (prevProps[key].height !== nextProps[key].height) return false;
+      } else {
+        if (!Object.prototype.hasOwnProperty.call(nextProps, key)) return false;
+        if (!Object.is(prevProps[key], nextProps[key])) return false;
+      }
+    }
+
+    return true;
+  }
+);
+
+const SizedApp = sizeMe({ monitorHeight: true })(App);
 
 App.displayName = 'App';
-
-const SizedApp: FunctionComponent<Omit<ComponentProps<typeof App>, 'width' | 'height'>> = (
-  props
-) => (
-  <SizeMe monitorHeight>
-    {({ size }) => (
-      // Don't pass size directly, because it's a new object each time.
-      <App {...props} {...size} />
-    )}
-  </SizeMe>
-);
 
 export default SizedApp;
