512e0949f78c3acbc37218307e52a828638d0416
ThibaudAv <thibaud.avenier@gmail.com>
2020-11-26 14:38:09 +0100

    refactor: rework story's props association with the component's properties
    - allow story with component to override local properties
    - handles Input with `bindingPropertyName`

--- app/angular/src/client/preview/angular-beta/ComponentClassFromStoryComponent.ts
+++ app/angular/src/client/preview/angular-beta/ComponentClassFromStoryComponent.ts
@@ -14,35 +14,28 @@ import { map, skip } from 'rxjs/operators';
 
 import { ICollection } from '../types';
 import { STORY_PROPS } from './app.token';
+import {
+  ComponentInputsOutputs,
+  getComponentDecoratorMetadata,
+  getComponentInputsOutputs,
+} from './NgComponentAnalyzer';
 import { RenderNgAppService } from './RenderNgAppService';
 
-const findComponentDecoratorMetadata = (component: any) => {
-  const decoratorKey = '__annotations__';
-  const decorators: any[] =
-    Reflect &&
-    Reflect.getOwnPropertyDescriptor &&
-    Reflect.getOwnPropertyDescriptor(component, decoratorKey)
-      ? Reflect.getOwnPropertyDescriptor(component, decoratorKey).value
-      : (component as any)[decoratorKey];
-
-  const ngComponentDecorator: Component | undefined = decorators.find(
-    (decorator) => decorator instanceof Component
-  );
-
-  return ngComponentDecorator;
-};
-
-const toInputsOutputs = (props: ICollection = {}) => {
-  return Object.entries(props).reduce(
-    (previousValue, [key, value]) => {
-      if (typeof value === 'function') {
-        return { ...previousValue, outputs: { ...previousValue.outputs, [key]: value } };
-      }
-
-      return { ...previousValue, inputs: { ...previousValue.inputs, [key]: value } };
-    },
-    { inputs: {}, outputs: {} } as { inputs: Record<string, any>; outputs: Record<string, any> }
-  );
+const getNamesOfInputsOutputsDefinedInProps = (
+  ngComponentInputsOutputs: ComponentInputsOutputs,
+  props: ICollection = {}
+) => {
+  const inputs = ngComponentInputsOutputs.inputs
+    .filter((i) => i.templateName in props)
+    .map((i) => i.templateName);
+  const outputs = ngComponentInputsOutputs.outputs
+    .filter((o) => o.templateName in props)
+    .map((o) => o.templateName);
+  return {
+    inputs,
+    outputs,
+    otherProps: Object.keys(props).filter((k) => ![...inputs, ...outputs].includes(k)),
+  };
 };
 
 /**
@@ -55,16 +48,17 @@ export const createComponentClassFromStoryComponent = (
   component: any,
   initialProps?: ICollection
 ): Type<any> => {
-  const ngComponentMetadata = findComponentDecoratorMetadata(component);
+  const ngComponentMetadata = getComponentDecoratorMetadata(component);
+  const ngComponentInputsOutputs = getComponentInputsOutputs(component);
 
-  const { inputs: initialInputs, outputs: initialOutputs } = toInputsOutputs(initialProps);
+  const {
+    inputs: initialInputs,
+    outputs: initialOutputs,
+    otherProps: initialOtherProps,
+  } = getNamesOfInputsOutputsDefinedInProps(ngComponentInputsOutputs, initialProps);
 
-  const templateInputs = Object.keys(initialInputs)
-    .map((i) => `[${i}]="${i}"`)
-    .join(' ');
-  const templateOutputs = Object.keys(initialOutputs)
-    .map((i) => `(${i})="${i}($event)"`)
-    .join(' ');
+  const templateInputs = initialInputs.map((i) => `[${i}]="${i}"`).join(' ');
+  const templateOutputs = initialOutputs.map((i) => `(${i})="${i}($event)"`).join(' ');
 
   @Component({
     selector: RenderNgAppService.SELECTOR_STORYBOOK_WRAPPER,
@@ -91,12 +85,49 @@ export const createComponentClassFromStoryComponent = (
     }
 
     ngAfterViewInit(): void {
+      // Initializes properties that are not Inputs | Outputs
+      // Allows story props to override local component properties
+      initialOtherProps.forEach((p) => {
+        (this.storyComponentElementRef as any)[p] = initialProps[p];
+      });
+
+      // `markForCheck` the component in case this uses changeDetection: OnPush
+      // And then forces the `detectChanges`
+      this.storyComponentViewContainerRef.injector.get(ChangeDetectorRef).markForCheck();
+      this.changeDetectorRef.detectChanges();
+
       // Once target component has been initialized, the storyProps$ observable keeps target component inputs up to date
       this.storyPropsSubscription = this.storyProps$
-        .pipe(skip(1), map(toInputsOutputs))
-        .subscribe(({ inputs }) => {
+        .pipe(
+          skip(1),
+          map((props) => {
+            // removes component output in props
+            const outputsKeyToRemove = ngComponentInputsOutputs.outputs.map((o) => o.templateName);
+            return Object.entries(props).reduce(
+              (prev, [key, value]) => ({
+                ...prev,
+                ...(!outputsKeyToRemove.includes(key) && { [key]: value }),
+              }),
+              {} as ICollection
+            );
+          }),
+          map((props) => {
+            // In case a component uses an input with `bindingPropertyName` (ex: @Input('name'))
+            // find the value of the local propName in the component Inputs
+            // otherwise use the input key
+            return Object.entries(props).reduce((prev, [propKey, value]) => {
+              const input = ngComponentInputsOutputs.inputs.find((o) => o.templateName === propKey);
+
+              return {
+                ...prev,
+                ...(input ? { [input.propName]: value } : { [propKey]: value }),
+              };
+            }, {} as ICollection);
+          })
+        )
+        .subscribe((props) => {
           // Replace inputs with new ones from props
-          Object.assign(this.storyComponentElementRef, inputs);
+          Object.assign(this.storyComponentElementRef, props);
 
           // `markForCheck` the component in case this uses changeDetection: OnPush
           // And then forces the `detectChanges`
