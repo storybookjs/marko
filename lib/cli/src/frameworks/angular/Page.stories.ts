84a4bde3f0c864a2a2501a020aba1212b461a848
Norbert de Langen <ndelangen@me.com>
2020-12-09 14:42:24 +0100

    FIX

--- lib/cli/src/frameworks/angular/Page.stories.ts
+++ lib/cli/src/frameworks/angular/Page.stories.ts
@@ -1,7 +1,5 @@
-import { moduleMetadata } from '@storybook/angular';
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/angular/types-6-0';
 
 import Button from './button.component';
 import Header from './header.component';
