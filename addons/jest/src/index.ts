e23f2d55931bd843d2beb7fad6b0cd58dac37b04
Dominic Lee <dominictwlee@gmail.com>
2020-12-29 13:56:07 +0000

    feat: infer tests from story filename

--- addons/jest/src/index.ts
+++ addons/jest/src/index.ts
@@ -57,12 +57,20 @@ export const withTests = (userOptions: { results: any; filesExt?: string }) => {
     const [storyFn, { kind, parameters = {} }] = args;
     let { jest: testFiles } = parameters;
 
+    if (Object.prototype.hasOwnProperty.call(testFiles, 'disabled') && testFiles.disabled) {
+      return storyFn();
+    }
+
     if (typeof testFiles === 'string') {
       testFiles = [testFiles];
     }
 
     if (testFiles && Array.isArray(testFiles)) {
       emitAddTests({ kind, story: storyFn, testFiles, options });
+    } else {
+      const { fileName: filePath } = parameters;
+      const fileName = filePath.split('/').pop().split('.')[0];
+      emitAddTests({ kind, story: storyFn, testFiles: [fileName], options });
     }
 
     return storyFn();
