4c07b2738a32eb6833cb2ef796134af28f368205
Dominic Lee <dominictwlee@gmail.com>
2020-12-31 19:02:23 +0000

    feat: copy over Files component from knobs

---
+++ lib/components/src/controls/Files.tsx
@@ -0,0 +1,46 @@
+import { FileReader } from 'global';
+import React, { ChangeEvent, FunctionComponent } from 'react';
+import { styled } from '@storybook/theming';
+import { ControlProps } from './types';
+
+import { Form } from '../form';
+
+export interface FilesControlProps extends ControlProps<string[]> {
+  accept: string;
+}
+
+const FileInput = styled(Form.Input)({
+  paddingTop: 4,
+});
+
+function fileReaderPromise(file: File) {
+  return new Promise<string>((resolve) => {
+    const fileReader = new FileReader();
+    fileReader.onload = (e: Event) => resolve((e.currentTarget as FileReader).result as string);
+    fileReader.readAsDataURL(file);
+  });
+}
+
+const serialize = (): undefined => undefined;
+const deserialize = (): undefined => undefined;
+
+const FilesControl: FunctionComponent<FilesControlProps> = ({ onChange, name, accept }) => (
+  <FileInput
+    type="file"
+    name={name}
+    multiple
+    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        Promise.all(Array.from(e.target.files).map(fileReaderPromise)).then(onChange);
+      }
+    }}
+    accept={accept}
+    size="flex"
+  />
+);
+
+FilesControl.defaultProps = {
+  onChange: (value) => value,
+};
+
+export default FilesControl;
