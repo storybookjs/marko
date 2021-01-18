7f7737345f83d052a474a9d2825f950551d2943e
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-01-18 14:54:20 -0700

    feat: check for addon-postcss before adding css loaders

--- lib/core/src/server/preview/base-webpack.config.ts
+++ lib/core/src/server/preview/base-webpack.config.ts
@@ -3,6 +3,38 @@ import findUp from 'find-up';
 import path from 'path';
 import { logger } from '@storybook/node-logger';
 
+// TODO(blaine): Deprecate this for the addon-postcss
+// Deprecating this will be a good way to cover all of the implicit usage of postcss-loader
+async function getPostcssOptions() {
+  const postcssConfigFiles = [
+    '.postcssrc',
+    '.postcssrc.json',
+    '.postcssrc.yml',
+    '.postcssrc.js',
+    'postcss.config.js',
+  ];
+  const customPostcssConfig = await findUp(postcssConfigFiles);
+
+  if (customPostcssConfig) {
+    // TODO(blaine): Deprecate this and utilize the natural config lookup in postcss-loader
+    logger.info(`=> Using custom ${path.basename(customPostcssConfig)}`);
+    return {
+      config: customPostcssConfig,
+    };
+  }
+  return {
+    // Additional config is merged with config, so we have it disabled currently
+    // TODO(blaine): Utilize the natural config lookup in postcss-loader
+    config: false,
+    plugins: () => [
+      require('postcss-flexbugs-fixes'), // eslint-disable-line global-require
+      autoprefixer({
+        flexbox: 'no-2009',
+      }),
+    ],
+  };
+}
+
 export async function createDefaultWebpackConfig(
   storybookBaseConfig: any,
   options: { presetsList: any[] }
@@ -15,32 +47,32 @@ export async function createDefaultWebpackConfig(
     return storybookBaseConfig;
   }
 
-  const postcssConfigFiles = [
-    '.postcssrc',
-    '.postcssrc.json',
-    '.postcssrc.yml',
-    '.postcssrc.js',
-    'postcss.config.js',
-  ];
-  const customPostcssConfig = await findUp(postcssConfigFiles);
+  // This pattern covers @storybook/addons-postcss and
+  // /storybook/addons/postcss (for local development)
+  const hasPostcssAddon = options.presetsList.some((preset) =>
+    /@storybook(\/|\\)addon-postcss/.test(preset.name || preset)
+  );
 
-  let postcssOptions = {};
-  if (customPostcssConfig) {
-    // TODO: Deprecate this and utilize the natural config lookup in postcss-loader
-    logger.info(`=> Using custom ${path.basename(customPostcssConfig)}`);
-    postcssOptions = {
-      config: customPostcssConfig,
-    };
-  } else {
-    postcssOptions = {
-      // Additional config is merged with config, so we have it disabled currently
-      // TODO: Utilize the natural config lookup in postcss-loader
-      config: false,
-      plugins: () => [
-        require('postcss-flexbugs-fixes'), // eslint-disable-line global-require
-        autoprefixer({
-          flexbox: 'no-2009',
-        }),
+  let cssLoaders = {};
+  if (!hasPostcssAddon) {
+    logger.info(`=> Using implicit CSS loaders`);
+    cssLoaders = {
+      test: /\.css$/,
+      sideEffects: true,
+      use: [
+        require.resolve('style-loader'),
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            importLoaders: 1,
+          },
+        },
+        {
+          loader: require.resolve('postcss-loader'),
+          options: {
+            postcssOptions: await getPostcssOptions(),
+          },
+        },
       ],
     };
   }
@@ -51,23 +83,7 @@ export async function createDefaultWebpackConfig(
       ...storybookBaseConfig.module,
       rules: [
         ...storybookBaseConfig.module.rules,
-        {
-          test: /\.css$/,
-          sideEffects: true,
-          use: [
-            require.resolve('style-loader'),
-            {
-              loader: require.resolve('css-loader'),
-              options: {
-                importLoaders: 1,
-              },
-            },
-            {
-              loader: require.resolve('postcss-loader'),
-              options: { postcssOptions },
-            },
-          ],
-        },
+        cssLoaders,
         {
           test: /\.(svg|ico|jpg|jpeg|png|apng|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/,
           loader: require.resolve('file-loader'),
