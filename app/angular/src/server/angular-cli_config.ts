1d0526db239155f57a88a85754985edfd4f9301a
Michael Shilman <michael@lab80.co>
2021-01-22 22:03:02 +0800

    Merge pull request #13704 from storybookjs/feat-add-angular-11-support

--- app/angular/src/server/angular-cli_config.ts
+++ app/angular/src/server/angular-cli_config.ts
@@ -125,7 +125,10 @@ export function getAngularCliWebpackConfigOptions(dirToSearch: Path) {
     supportES2015: false,
     buildOptions: {
       sourceMap: false,
-      optimization: {},
+      optimization: {
+        styles: true,
+        scripts: true,
+      },
       ...projectOptions,
       assets: normalizedAssets,
       budgets,
