e28dedb25bf956d95e8b3774706e910116276693
Norbert de Langen <ndelangen@me.com>
2021-02-04 17:41:55 +0100

    cleanup

--- lib/builder-webpack4/src/preview/iframe-webpack.config.ts
+++ lib/builder-webpack4/src/preview/iframe-webpack.config.ts
@@ -21,6 +21,7 @@ import { useBaseTsSupport } from '../utils/useBaseTsSupport';
 import { loadEnv } from '../utils/envs';
 import { nodeModulesPaths } from '../utils/paths';
 import { es6Transpiler } from '../utils/es6Transpiler';
+import { interpolate } from '../utils/interpolate';
 
 const storybookPaths: Record<string, string> = [
   'addons',
@@ -227,19 +228,4 @@ export default async ({
       hints: isProd ? 'warning' : false,
     },
   };
-};
-
-/**
- * Return a string corresponding to template filled with bindings using following pattern:
- * For each (key, value) of `bindings` replace, in template, `{{key}}` by escaped version of `value`
- *
- * @param template {String} Template with `{{binding}}`
- * @param bindings {Object} key-value object use to fill the template, `{{key}}` will be replaced by `escaped(value)`
- * @returns {String} Filled template
- */
-const interpolate = (template: string, bindings: Record<string, string>) => {
-  return Object.entries(bindings).reduce((acc, [k, v]) => {
-    const escapedString = v.replace(/\\/g, '/').replace(/\$/g, '$$$');
-    return acc.replace(new RegExp(`{{${k}}}`, 'g'), escapedString);
-  }, template);
 };
