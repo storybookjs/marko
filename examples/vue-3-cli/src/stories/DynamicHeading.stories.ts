a006e0440bad8a298d3f371fcff6602d303e4fb2
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-02-09 13:47:40 -0700

    chore: add functional TS component/story to vue 3 example

---
+++ examples/vue-3-cli/src/stories/DynamicHeading.stories.ts
@@ -0,0 +1,53 @@
+import { Story } from '@storybook/vue3';
+import { FunctionalComponent, h } from 'vue';
+import DynamicHeading, { Props } from './DynamicHeading';
+
+export default {
+  title: 'Example/DynamicHeading',
+  component: DynamicHeading,
+  argTypes: {
+    // Number type is detected, but we still want to constrain the range from 1-6
+    level: { control: { min: 1, max: 6 } },
+  },
+};
+
+/*
+  You can return a Vue 3 functional component from a Story.
+
+  Make sure to specify the `props` the component expects to receive!
+ */
+const Template: Story = (args, { argTypes }) => {
+  const component: FunctionalComponent<Props> = (props) => h(DynamicHeading, props, 'Hello World!');
+  component.props = Object.keys(argTypes);
+  return component;
+};
+
+export const One = Template.bind({});
+One.args = {
+  level: 1,
+};
+
+export const Two = Template.bind({});
+Two.args = {
+  level: 2,
+};
+
+export const Three = Template.bind({});
+Three.args = {
+  level: 3,
+};
+
+export const Four = Template.bind({});
+Four.args = {
+  level: 4,
+};
+
+export const Five = Template.bind({});
+Five.args = {
+  level: 5,
+};
+
+export const Six = Template.bind({});
+Six.args = {
+  level: 6,
+};
