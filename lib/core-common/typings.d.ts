40d1c3681836637d4c2611fb589537772b8a23b3
Norbert de Langen <ndelangen@me.com>
2021-02-12 20:03:15 +0100

    cleanup

--- lib/core-common/typings.d.ts
+++ lib/core-common/typings.d.ts
@@ -1,12 +1,5 @@
-declare module 'global';
-declare module '@storybook/semver';
-declare module 'unfetch/dist/unfetch';
 declare module 'lazy-universal-dotenv';
 declare module 'pnp-webpack-plugin';
-declare module '@storybook/theming/paths';
-declare module '@storybook/ui/paths';
-declare module 'better-opn';
-declare module '@storybook/ui';
 
 declare module 'file-system-cache' {
   export interface Options {
