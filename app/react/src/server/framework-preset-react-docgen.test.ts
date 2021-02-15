925dfb95a9a75e37830162de3cdb4f0f12a5f3ff
Norbert de Langen <ndelangen@me.com>
2021-02-15 10:45:06 +0100

    fix test

--- app/react/src/server/framework-preset-react-docgen.test.ts
+++ app/react/src/server/framework-preset-react-docgen.test.ts
@@ -1,82 +1,108 @@
-// TODO: fix after change to use presets.apply to get the typescriptOptions
+import ReactDocgenTypescriptPlugin from 'react-docgen-typescript-plugin';
+import type { Options, TypescriptConfig } from '@storybook/core-common';
+import * as preset from './framework-preset-react-docgen';
 
-// import ReactDocgenTypescriptPlugin from 'react-docgen-typescript-plugin';
-// import type { Options } from '@storybook/core-common';
-// import * as preset from './framework-preset-react-docgen';
+describe('framework-preset-react-docgen', () => {
+  const babelPluginReactDocgenPath = require.resolve('babel-plugin-react-docgen');
 
-// describe('framework-preset-react-docgen', () => {
-//   const babelPluginReactDocgenPath = require.resolve('babel-plugin-react-docgen');
+  it('should return the babel config with the extra plugin', async () => {
+    const babelConfig = {
+      babelrc: false,
+      presets: ['env', 'foo-preset'],
+      plugins: ['foo-plugin'],
+    };
 
-//   it('should return the babel config with the extra plugin', () => {
-//     const babelConfig = {
-//       babelrc: false,
-//       presets: ['env', 'foo-preset'],
-//       plugins: ['foo-plugin'],
-//     };
+    const config = await preset.babel(babelConfig, {
+      presets: {
+        // @ts-ignore
+        apply: async () =>
+          ({
+            check: false,
+            reactDocgen: 'react-docgen',
+          } as TypescriptConfig),
+      },
+    } as any);
 
-//     const config = preset.babel(babelConfig, {
-//       typescriptOptions: { check: false, reactDocgen: 'react-docgen' },
-//     } as Options);
+    expect(config).toEqual({
+      babelrc: false,
+      plugins: ['foo-plugin'],
+      presets: ['env', 'foo-preset'],
+      overrides: [
+        {
+          test: /\.(mjs|tsx?|jsx?)$/,
+          plugins: [
+            [
+              babelPluginReactDocgenPath,
+              {
+                DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES',
+              },
+            ],
+          ],
+        },
+      ],
+    });
+  });
 
-//     expect(config).toEqual({
-//       babelrc: false,
-//       plugins: ['foo-plugin'],
-//       presets: ['env', 'foo-preset'],
-//       overrides: [
-//         {
-//           test: /\.(mjs|tsx?|jsx?)$/,
-//           plugins: [
-//             [
-//               babelPluginReactDocgenPath,
-//               {
-//                 DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES',
-//               },
-//             ],
-//           ],
-//         },
-//       ],
-//     });
-//   });
+  it('should return the webpack config with the extra plugin', async () => {
+    const webpackConfig = {
+      plugins: [],
+    };
 
-//   it('should return the webpack config with the extra plugin', () => {
-//     const webpackConfig = {
-//       plugins: [],
-//     };
+    const config = await preset.webpackFinal(webpackConfig, {
+      presets: {
+        // @ts-ignore
+        apply: async () =>
+          ({
+            check: false,
+            reactDocgen: 'react-docgen-typescript',
+          } as TypescriptConfig),
+      },
+    });
 
-//     const config = preset.webpackFinal(webpackConfig, {
-//       typescriptOptions: { check: false, reactDocgen: 'react-docgen-typescript' },
-//     });
+    expect(config).toEqual({
+      plugins: [expect.any(ReactDocgenTypescriptPlugin)],
+    });
+  });
 
-//     expect(config).toEqual({
-//       plugins: [expect.any(ReactDocgenTypescriptPlugin)],
-//     });
-//   });
+  it('should not add any extra plugins', async () => {
+    const babelConfig = {
+      babelrc: false,
+      presets: ['env', 'foo-preset'],
+      plugins: ['foo-plugin'],
+    };
 
-//   it('should not add any extra plugins', () => {
-//     const babelConfig = {
-//       babelrc: false,
-//       presets: ['env', 'foo-preset'],
-//       plugins: ['foo-plugin'],
-//     };
+    const webpackConfig = {
+      plugins: [],
+    };
 
-//     const webpackConfig = {
-//       plugins: [],
-//     };
+    const outputBabelconfig = await preset.babel(babelConfig, {
+      presets: {
+        // @ts-ignore
+        apply: async () =>
+          ({
+            check: false,
+            reactDocgen: false,
+          } as TypescriptConfig),
+      },
+    });
+    const outputWebpackconfig = await preset.webpackFinal(webpackConfig, {
+      presets: {
+        // @ts-ignore
+        apply: async () =>
+          ({
+            check: false,
+            reactDocgen: false,
+          } as TypescriptConfig),
+      },
+    });
 
-//     const outputBabelconfig = preset.babel(babelConfig, {
-//       typescriptOptions: { check: false, reactDocgen: false },
-//     });
-//     const outputWebpackconfig = preset.webpackFinal(webpackConfig, {
-//       typescriptOptions: { check: false, reactDocgen: false },
-//     });
-
-//     expect(outputBabelconfig).toEqual({
-//       babelrc: false,
-//       presets: ['env', 'foo-preset'],
-//       plugins: ['foo-plugin'],
-//     });
-//     expect(outputWebpackconfig).toEqual({
-//       plugins: [],
-//     });
-//   });
-// });
+    expect(outputBabelconfig).toEqual({
+      babelrc: false,
+      presets: ['env', 'foo-preset'],
+      plugins: ['foo-plugin'],
+    });
+    expect(outputWebpackconfig).toEqual({
+      plugins: [],
+    });
+  });
+});
