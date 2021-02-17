bd8848b50354c3ea550b773ac094525ae96db59f
Tom Coleman <tom@thesnail.org>
2021-02-17 20:11:08 +1100

    Don't set argType.defaultValue to `null` when it should be `undefined`.

--- addons/docs/src/frameworks/react/extractArgTypes.ts
+++ addons/docs/src/frameworks/react/extractArgTypes.ts
@@ -8,10 +8,16 @@ export const extractArgTypes: ArgTypesExtractor = (component) => {
     if (rows) {
       return rows.reduce((acc: ArgTypes, row: PropDef) => {
         const { type, sbType, defaultValue: defaultSummary, jsDocTags, required } = row;
-        let defaultValue = defaultSummary && (defaultSummary.detail || defaultSummary.summary);
+        let defaultValue;
+
+        if (defaultSummary) {
+          defaultValue = defaultSummary.detail || defaultSummary.summary;
+        }
         try {
-          // eslint-disable-next-line no-eval
-          defaultValue = eval(defaultValue);
+          if (defaultValue) {
+            // eslint-disable-next-line no-eval
+            defaultValue = eval(defaultValue);
+          }
           // eslint-disable-next-line no-empty
         } catch {}
 
