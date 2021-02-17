c0148661141fd79ada82beb91c86225fc10334a5
Tom Coleman <tom@thesnail.org>
2021-02-17 19:42:05 +1100

    Prefer the *actual* default prop if we know it!

--- addons/docs/src/frameworks/react/extractArgTypes.ts
+++ addons/docs/src/frameworks/react/extractArgTypes.ts
@@ -7,17 +7,22 @@ export const extractArgTypes: ArgTypesExtractor = (component) => {
     const { rows } = extractProps(component);
     if (rows) {
       return rows.reduce((acc: ArgTypes, row: PropDef) => {
-        const { type, sbType, defaultValue: defaultSummary, jsDocTags, required } = row;
+        const { name, type, sbType, defaultValue: defaultSummary, jsDocTags, required } = row;
+
         let defaultValue;
-        const defaultValueString =
-          defaultSummary && (defaultSummary.detail || defaultSummary.summary);
-        try {
-          if (defaultValueString) {
-            // eslint-disable-next-line no-new-func
-            defaultValue = Function(`"use strict";return (${defaultValueString})`)();
-          }
-          // eslint-disable-next-line no-empty
-        } catch {}
+        if (component.defaultProps) {
+          defaultValue = component.defaultProps[name];
+        } else {
+          const defaultValueString =
+            defaultSummary && (defaultSummary.detail || defaultSummary.summary);
+          try {
+            if (defaultValueString) {
+              // eslint-disable-next-line no-new-func
+              defaultValue = Function(`"use strict";return (${defaultValueString})`)();
+            }
+            // eslint-disable-next-line no-empty
+          } catch {}
+        }
 
         acc[row.name] = {
           ...row,
