52479887c73ec49b318291a52fb990ab76b9c6e8
Michael Shilman <michael@lab80.co>
2021-02-22 08:40:23 +0800

    Addon-docs: Support story.mdx, stories.mdx

--- examples/official-storybook/main.ts
+++ examples/official-storybook/main.ts
@@ -7,7 +7,7 @@ module.exports = {
     // FIXME: Breaks e2e tests './intro.stories.mdx',
     '../../lib/ui/src/**/*.stories.@(js|tsx|mdx)',
     '../../lib/components/src/**/*.stories.@(js|tsx|mdx)',
-    './stories/**/*.stories.@(js|ts|tsx|mdx)',
+    './stories/**/*stories.@(js|ts|tsx|mdx)',
     './../../addons/docs/**/*.stories.tsx',
   ],
   reactOptions: {
