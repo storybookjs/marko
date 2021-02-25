3c2707cc2c7dfb7ce5e07b86aff2cb85427a90bc
Michael Shilman <shilman@users.noreply.github.com>
2021-02-26 07:25:05 +0800

    Merge pull request #14047 from storybookjs/pocka/fix/transpile-prettier
    IE11: Transpile prettier down to ES5

--- lib/core/src/server/common/es6Transpiler.ts
+++ lib/core/src/server/common/es6Transpiler.ts
@@ -4,7 +4,7 @@ import { plugins } from './babel';
 export const es6Transpiler: () => RuleSetRule = () => {
   // TODO: generate regexp using are-you-es5
 
-  const include = /[\\/]node_modules[\\/](@storybook\/node-logger|are-you-es5|better-opn|boxen|chalk|commander|find-cache-dir|find-up|fs-extra|json5|node-fetch|pkg-dir|resolve-from|semver)/;
+  const include = /[\\/]node_modules[\\/](@storybook\/node-logger|are-you-es5|better-opn|boxen|chalk|commander|find-cache-dir|find-up|fs-extra|json5|node-fetch|pkg-dir|prettier|resolve-from|semver)/;
   return {
     test: /\.js$/,
     use: [
