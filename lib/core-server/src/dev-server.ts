777b489524ae83ed4e05e914d7d54c6972c079c9
Kaelig <kaelig.deloumeauprigent@shopify.com>
2021-04-02 16:40:43 -0700

    Enable gzip compression on the development server

--- lib/core-server/src/dev-server.ts
+++ lib/core-server/src/dev-server.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import compression from 'compression';
 
 import { Builder, logConfig, Options } from '@storybook/core-common';
 
@@ -19,6 +20,8 @@ export async function storybookDevServer(options: Options) {
   const startTime = process.hrtime();
   const app = express();
   const server = await getServer(app, options);
+
+  app.use(compression({ level: 1 }));
 
   if (typeof options.extendServer === 'function') {
     options.extendServer(server);
