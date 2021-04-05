6f82a835816c0ea683a39163a74918c87589b1d2
Klemen Slavič <krof.drakula@gmail.com>
2021-04-05 08:21:15 +0200

    Switch renderTree to use preact-render-to-string instead

--- addons/storyshots/storyshots-core/src/frameworks/preact/renderTree.ts
+++ addons/storyshots/storyshots-core/src/frameworks/preact/renderTree.ts
@@ -1,11 +1,14 @@
 /** @jsx h */
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import preactRenderer from 'preact-render-to-json';
+import preactRenderer from 'preact-render-to-string';
+
+const boundRenderer = (_storyElement: any, _rendererOptions: any) =>
+  preactRenderer(_storyElement, null, { pretty: '  ' });
 
 function getRenderedTree(story: any, context: any, { renderer, ...rendererOptions }: any) {
   const storyElement = story.render();
-  const currentRenderer = renderer || preactRenderer;
+  const currentRenderer = renderer || boundRenderer;
   const tree = currentRenderer(storyElement, rendererOptions);
 
   return tree;
