10ce00b5cdbf0ea0cede0642a698024ade57c747
Klemen Slavič <krof.drakula@gmail.com>
2021-04-05 09:46:10 +0200

    Fix module import typing problem

--- addons/storyshots/storyshots-core/src/typings.d.ts
+++ addons/storyshots/storyshots-core/src/typings.d.ts
@@ -1,6 +1,6 @@
 declare module 'global';
 declare module 'jest-preset-angular/*';
-declare module 'preact-render-to-json';
+declare module 'preact-render-to-string/jsx';
 declare module 'react-test-renderer*';
 declare module 'rax-test-renderer*';
 declare module 'babel-plugin-require-context-hook/register';
