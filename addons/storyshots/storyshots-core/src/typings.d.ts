2213333321fb6d065403241f5d7f16e20fdf2fe9
Klemen Slavič <krof.drakula@gmail.com>
2021-04-05 11:22:39 +0200

    Fix typings file

--- addons/storyshots/storyshots-core/src/typings.d.ts
+++ addons/storyshots/storyshots-core/src/typings.d.ts
@@ -1,6 +1,6 @@
 declare module 'global';
 declare module 'jest-preset-angular/*';
-declare module 'preact-render-to-string/jsx';
+declare module 'preact-render-to-string';
 declare module 'react-test-renderer*';
 declare module 'rax-test-renderer*';
 declare module 'babel-plugin-require-context-hook/register';
