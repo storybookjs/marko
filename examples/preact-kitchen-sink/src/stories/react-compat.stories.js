130bfae1ba621cd4bc8a57fcb5d9f0297d9c4e0d
Klemen Slavič <krof.drakula@gmail.com>
2021-04-12 14:17:55 +0200

    Fix preact storyshoot in compat mode

--- examples/preact-kitchen-sink/src/stories/react-compat.stories.js
+++ examples/preact-kitchen-sink/src/stories/react-compat.stories.js
@@ -1,48 +1,9 @@
 /** @jsx h */
-
 import { h } from 'preact';
-import { createElement, Component, useState } from 'react';
-import PropTypes from 'prop-types';
+import { ReactFunctionalComponent, ReactClassComponent } from '../React';
 
 export default {
   title: 'React Compatibility',
-};
-
-const ReactFunctionalComponent = ({ label }) => {
-  const [clicks, setValue] = useState(0);
-  return createElement(
-    'div',
-    { onClick: () => setValue(clicks + 1) },
-    createElement('div', { style: { color: 'red' } }, label),
-    createElement('div', {}, `Clicked ${clicks} times.`)
-  );
-};
-
-ReactFunctionalComponent.propTypes = {
-  label: PropTypes.string.isRequired,
-};
-
-class ReactClassComponent extends Component {
-  state = {
-    clicks: 0,
-  };
-
-  render() {
-    const { label } = this.props;
-    const { clicks } = this.state;
-    return createElement(
-      'div',
-      {
-        onClick: () => this.setState({ clicks: clicks + 1 }),
-      },
-      createElement('div', { style: { color: 'green' } }, label),
-      createElement('div', null, `Clicked ${clicks} times.`)
-    );
-  }
-}
-
-ReactClassComponent.propTypes = {
-  label: PropTypes.string.isRequired,
 };
 
 export const ReactComponentDemo = () => (
