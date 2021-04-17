def4fb743c11523ff932fdbdbbfa7e7959c90aa0
peterpeterparker <david.dalbusco@outlook.com>
2021-04-17 14:25:08 +0200

    docs: missing dash in place-hold.it url

--- lib/components/src/blocks/Description.stories.tsx
+++ lib/components/src/blocks/Description.stories.tsx
@@ -13,7 +13,7 @@ const markdownCaption = `
 
 The group looked like tall, exotic grazing animals, swaying gracefully and unconsciously with the movement of the train, their high heels like polished hooves against the gray metal of the Flatline as a construct, a hardwired ROM cassette replicating a dead man’s skills, obsessions, kneejerk responses.
 
-![An image](http://placehold.it/350x150)
+![An image](http://place-hold.it/350x150)
 
 He stared at the clinic, Molly took him to the Tank War, mouth touched with hot gold as a gliding cursor struck sparks from the wall of a skyscraper canyon.
 `;
