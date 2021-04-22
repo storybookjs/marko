9c2a1c67a1487ca4711569c0ee5ed2b4854a79fd
Tom Coleman <tom@thesnail.org>
2021-04-22 21:31:37 +1000

    Some minor comment improvements

--- lib/client-api/src/decorators.ts
+++ lib/client-api/src/decorators.ts
@@ -26,7 +26,7 @@ export const decorateStory = (
 ): LegacyStoryFn => {
   // Bind the partially decorated storyFn so that when it is called it always knows about the story context,
   // no matter what it is passed directly. This is because we cannot guarantee a decorator will
-  // pass the context down to the next decorator in the chain.
+  // pass the context down to the next decorated story in the chain.
   const boundStoryFunction = bindWithContext(storyFn, getStoryContext);
 
   return (context: StoryContext) => decorator(boundStoryFunction, context);
@@ -40,7 +40,8 @@ export const defaultDecorateStory = (
   // Instead we pass it a context "getter", which is defined once (at "decoration time")
   // The getter reads a variable which is scoped to this call of `decorateStory`
   // (ie to this story), so there is no possibility of overlap.
-  // This will break if you call the same story twice interleaved.
+  // This will break if you call the same story twice interleaved
+  // (React might do it if you rendered the same story twice in the one ReactDom.render call, for instance)
   let contextStore: StoryContext;
   const decoratedWithContextStore = decorators.reduce(
     (story, decorator) => decorateStory(story, decorator, () => contextStore),
@@ -48,6 +49,6 @@ export const defaultDecorateStory = (
   );
   return (context) => {
     contextStore = context;
-    return decoratedWithContextStore(context);
+    return decoratedWithContextStore(context); // Pass the context directly into the first decorator
   };
 };
