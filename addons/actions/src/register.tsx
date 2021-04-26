0b5b004dd21441abc1cb300d6c7afe9a0341ede0
Angela Papadopoulou <angelikeepap@gmail.com>
2021-04-27 01:06:54 +0300

    Addon-actions: Add 'New Action' indicator

--- addons/actions/src/register.tsx
+++ addons/actions/src/register.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { addons, types } from '@storybook/addons';
+import { STORY_CHANGED } from '@storybook/core-events';
 import ActionLogger from './containers/ActionLogger';
-import { ADDON_ID, PANEL_ID, PARAM_KEY } from './constants';
+import { ADDON_ID, EVENT_ID, PANEL_ID, PARAM_KEY } from './constants';
 
 addons.register(ADDON_ID, (api) => {
   addons.addPanel(PANEL_ID, {
-    title: 'Actions',
+    title() {
+      const [actionsCount, setActionsCount] = React.useState(0);
+      api.on(EVENT_ID, () => {
+        setActionsCount(actionsCount + 1);
+      });
+      api.on(STORY_CHANGED, () => setActionsCount(0));
+      const suffix = actionsCount === 0 ? '' : ` (${actionsCount})`;
+      return `Actions${suffix}`;
+    },
     type: types.PANEL,
     render: ({ active, key }) => <ActionLogger key={key} api={api} active={active} />,
     paramKey: PARAM_KEY,
