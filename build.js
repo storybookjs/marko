e5c769c833bd5f874fe3b7c7dd34289ac59e3422
Dylan Piercey <dpiercey@ebay.com>
2021-06-09 10:52:56 -0700

    fix: ensure .marko files copied to dist folder

--- build.js
+++ build.js
@@ -1,7 +1,11 @@
+const fs = require("fs");
+const path = require("path");
 const glob = require("tiny-glob");
 const { build } = require("esbuild");
+const srcdir = path.resolve("src");
 
 (async () => {
+  const markoFiles = await glob("src/{bin,client,server}/**/*.marko");
   const entryPoints = [
     "src/testing.ts",
     "src/standalone.ts",
@@ -9,12 +13,21 @@ const { build } = require("esbuild");
   ];
 
   for (const format of ["esm", "cjs"]) {
-    await build({
-      format,
-      entryPoints,
-      outdir: `dist/${format}`,
-      platform: "node",
-      target: ["node14"],
-    });
+    const outdir = path.resolve("dist", format);
+    await Promise.all([
+      ...markoFiles.map((file) =>
+        fs.promises.copyFile(
+          file,
+          path.join(outdir, path.relative(srcdir, file))
+        )
+      ),
+      build({
+        format,
+        entryPoints,
+        outdir: `dist/${format}`,
+        platform: "node",
+        target: ["node14"],
+      }),
+    ]);
   }
 })().catch(() => process.exit(1));
