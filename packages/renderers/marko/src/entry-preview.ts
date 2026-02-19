6519ee29e91e6419be15ef3c1ce5350c2e64059a
LuLaValva <lukelavalva@gmail.com>
2026-02-18 16:18:27 -0800

    feat: auto-add change handler with description to table

--- packages/renderers/marko/src/entry-preview.ts
+++ packages/renderers/marko/src/entry-preview.ts
@@ -1,3 +1,38 @@
+import type { ArgTypesEnhancer, StrictArgTypes } from "storybook/internal/csf";
+import type { MarkoRenderer } from "./types";
+
 export { renderToCanvas, render } from "./render";
 
 export const parameters = { renderer: "marko" };
+
+export const argTypesEnhancers: ArgTypesEnhancer<MarkoRenderer>[] = [
+  ({ argTypes }) => {
+    const newTypes: StrictArgTypes = {};
+
+    for (const key of Object.keys(argTypes)) {
+      const argType = argTypes[key];
+
+      newTypes[key] = { ...argType };
+
+      const changeKey = key + "Change";
+      if (argType.changeHandler && !(changeKey in argTypes)) {
+        newTypes[changeKey] = {
+          name: changeKey,
+          description:
+            typeof argType.changeHandler === "string"
+              ? argType.changeHandler
+              : typeof argType.changeHandler === "function"
+                ? argType.changeHandler(key)
+                : `Used to hoist \`${key}\` with the [controllable](https://markojs.com/docs/explanation/controllable-components) pattern. Typically added implicitly with [the \`:=\` bind syntax](https://markojs.com/docs/reference/language#shorthand-change-handlers-two-way-binding).`,
+          table: {
+            type: {
+              summary: `(${key.charAt(0)}: ${argType.table?.type?.summary ?? "unknown"}) => void`,
+            },
+          },
+          control: { disable: true },
+        };
+      }
+    }
+    return newTypes;
+  },
+];
--- packages/renderers/marko/src/typings.d.ts
+++ packages/renderers/marko/src/typings.d.ts
@@ -7,3 +7,25 @@ declare const LOGLEVEL:
   | "error"
   | "silent"
   | undefined;
+
+// Empty import makes this file a module so "declare module" augments instead of replacing
+import type {} from "storybook/internal/csf";
+
+declare module "storybook/internal/csf" {
+  interface InputType {
+    /**
+     * **_[Marko]_**
+     *
+     * Indicates that this arg is [controllable](https://markojs.com/docs/explanation/controllable-components#the-controllable-pattern) via a `_Change` handler.
+     *
+     * Pass `true` to add an english-language description written by Marko team
+     *
+     * Pass a string to use it as the description
+     *
+     * Pass a function to build a description from this argType
+     *
+     * For further customization, add a `_Change` argType manually
+     */
+    changeHandler?: true | string | ((name: string) => string);
+  }
+}
